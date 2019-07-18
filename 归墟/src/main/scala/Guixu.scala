dd597ce05ff32da1f540b661e69c2a0fa936caa7
djx314 <djx314@sina.cn>
2019-07-18 13:17:13 +0800

    归墟

--- 归墟/src/main/scala/Guixu.scala
+++ 归墟/src/main/scala/Guixu.scala
@@ -4,34 +4,30 @@ class 水
 object 水 extends 水
 
 trait 归墟 {
-  type I <: 水
-  type T <: 归墟
-
-  val head: 水
-  val tail: T
-
-  type AddWater[I <: 水] <: 归墟
-  def add[I <: 水](i: I): AddWater[I]
-}
-
-class 归墟Impl extends 归墟 {
   self =>
-  override type I = 水
-  override type T = 归墟Impl
-  override val head: 水      = 水
-  override val tail: 归墟Impl = self
 
-  override type AddWater[I <: 水] = 归墟Impl
-  override def add[I <: 水](i: I): 归墟Impl = new 归墟Impl {
-    override type I = 水
-    override type T = 归墟Impl
-    override val head: 水      = i
-    override val tail: 归墟Impl = self
+  type I
+  type T = 归墟
+
+  val head: I
+  val tail: 归墟 = self
+
+  type AddWater[I] = 归墟
+  def add[II](i: II): AddWater[I] = new 归墟 {
+    override type I = II
+    override val head: II = i
+
+    override type T = 归墟
+    override val tail: 归墟 = self
 
     override def toString = head.toString + "," + self.toString
   }
 
-  override def toString = "HNil"
 }
 
-object 归墟Impl extends 归墟Impl
+object 归墟 extends 归墟 {
+
+  override type I = 水
+  override val head: 水 = 水
+
+}
