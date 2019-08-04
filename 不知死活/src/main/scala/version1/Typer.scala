3be57c5312a56e996857e57e7feee768c6e06131
djx314 <djx314@sina.cn>
2019-08-04 12:50:11 +0800

    不知死活

--- 不知死活/src/main/scala/version1/Typer.scala
+++ 不知死活/src/main/scala/version1/Typer.scala
@@ -2,27 +2,27 @@ package version1
 
 object Typer {
 
-type 测试水尺 = 初始水尺#下一重#下一重#下一重#下一重
-type 大海0  = 大海之初#加水[Int]#加水[String]
+  type 测试水尺 = 初始水尺#下一重#下一重#下一重#下一重
+  type 大海0  = 大海之初#加水[Int]#加水[String]
 
-type 舀水1 = 测试水尺#检验[大海0]
-type 大海1 = 舀水1#目前舀水#舀[大海0, String]
-val 大海1: 大海之初#加水[Int]#加水[String]#加水[String] = (throw new Exception("ii")): 大海1
+  type 舀水1 = 测试水尺#检验[大海0]
+  type 大海1 = 舀水1#目前舀水#舀[大海0, String]
+  val 大海1: 大海之初#加水[Int]#加水[String]#加水[String] = (throw new Exception("ii")): 大海1
 
-type 舀水2 = 测试水尺#检验[大海1]
-type 大海2 = 舀水2#目前舀水#舀[大海1, Int]
-val 大海2: 大海之初#加水[Int]#加水[String]#加水[String]#加水[Int] = (throw new Exception("ii")): 大海2
+  type 舀水2 = 测试水尺#检验[大海1]
+  type 大海2 = 舀水2#目前舀水#舀[大海1, Int]
+  val 大海2: 大海之初#加水[Int]#加水[String]#加水[String]#加水[Int] = (throw new Exception("ii")): 大海2
 
-type 舀水3 = 测试水尺#检验[大海2]
-type 大海3 = 舀水3#目前舀水#舀[大海2, Long]
-val 大海3: 大海之初#加水[Int]#加水[String]#加水[String]#加水[Int]#加水[Long] = (throw new Exception("ii")): 大海3
+  type 舀水3 = 测试水尺#检验[大海2]
+  type 大海3 = 舀水3#目前舀水#舀[大海2, Long]
+  val 大海3: 大海之初#加水[Int]#加水[String]#加水[String]#加水[Int]#加水[Long] = (throw new Exception("ii")): 大海3
 
-type 舀水4 = 测试水尺#检验[大海3]
-type 大海4 = 舀水4#目前舀水#舀[大海3, Short]
-val 大海4: 大海之初#加水[Int]#加水[String]#加水[String]#加水[Int]#加水[Long] = (throw new Exception("ii")): 大海4
+  type 舀水4 = 测试水尺#检验[大海3]
+  type 大海4 = 舀水4#目前舀水#舀[大海3, Short]
+  val 大海4: 大海之初#加水[Int]#加水[String]#加水[String]#加水[Int]#加水[Long] = (throw new Exception("ii")): 大海4
 
-type 舀水5 = 测试水尺#检验[大海4]
-type 大海5 = 舀水5#目前舀水#舀[大海4, String]
-val 大海5: 大海之初#加水[Int]#加水[String]#加水[String]#加水[Int]#加水[Long] = (throw new Exception("ii")): 大海5
+  type 舀水5 = 测试水尺#检验[大海4]
+  type 大海5 = 舀水5#目前舀水#舀[大海4, String]
+  val 大海5: 大海之初#加水[Int]#加水[String]#加水[String]#加水[Int]#加水[Long] = (throw new Exception("ii")): 大海5
 
 }
--- 不知死活/src/main/scala/version1/Yaoshui.scala
+++ 不知死活/src/main/scala/version1/Yaoshui.scala
@@ -4,19 +4,22 @@ import scala.language.higherKinds
 
 trait 舀水 {
   type 舀[H <: 大海, I] <: 大海
-  def 舀[H <: 大海, I](大海: H, 归墟: 归墟, 水: I): (舀[H, I], 归墟)
+  def 舀[H <: 大海, I](大海: H, 水: I): 舀[H, I]
 }
 
 class 没有满溢 extends 舀水 {
   self =>
   override type 舀[H <: 大海, I] = H#加水[I]
-  override def 舀[H <: 大海, I](大海: H, 归墟: 归墟, 水: I): (H#加水[I], 归墟) = (大海.加水(水), 归墟)
+  override def 舀[H <: 大海, I](大海: H, 水: I): H#加水[I] = 大海.加水(水)
 }
 object 没有满溢 extends 没有满溢
 
 class 满溢 extends 舀水 {
   self =>
   override type 舀[H <: 大海, I] = H
-  override def 舀[H <: 大海, I](大海: H, 归墟: 归墟, 水: I): (H, 归墟) = (大海, 归墟.加水(水))
+  override def 舀[H <: 大海, I](大海: H, 水: I): H = {
+    println(s"满水了, 水: " + 水 + " 被舀去了其他地方")
+    大海
+  }
 }
 object 满溢 extends 满溢
