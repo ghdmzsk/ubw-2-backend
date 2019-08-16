a5f618490ba2d838ea185994ce79f71fa28d1f44
djx314 <djx314@sina.cn>
2019-08-16 09:11:23 +0800

    不知火

--- 不知火/src/main/scala/Zuranwu.scala
+++ 不知火/src/main/scala/Zuranwu.scala
@@ -9,6 +9,9 @@ trait 阻燃物 {
   type 消失[T <: 柴] <: 柴
   def 消失[T <: 柴](t: T): 消失[T]
 
+  type 初始化 <: 阻燃物
+  def 初始化: 初始化
+
 }
 
 class 一般阻燃物[T <: 阻燃物](override val 尾: T) extends 阻燃物 {
@@ -19,19 +22,24 @@ class 一般阻燃物[T <: 阻燃物](override val 尾: T) extends 阻燃物 {
   override type 消失[T <: 柴] = 一块柴[T#尾, T#当前阻燃物#尾]
   override def 消失[T <: 柴](t: T): 一块柴[T#尾, T#当前阻燃物#尾] = new 一块柴(t.尾, t.当前阻燃物.尾)
 
+  override type 初始化 = 一般阻燃物[一般阻燃物[T]]
+  override def 初始化: 一般阻燃物[一般阻燃物[T]] = new 一般阻燃物(self)
 }
 
-class 初始阻燃物 extends 阻燃物 {
+class 灰烬 extends 阻燃物 {
   self =>
 
-  override type 尾 = 初始阻燃物
-  override def 尾: 初始阻燃物 = self
+  override type 尾 = 灰烬
+  override def 尾: 灰烬 = self
 
   override type 消失[T <: 柴] = T#尾
   override def 消失[T <: 柴](t: T): T#尾 = t.尾
 
+  override type 初始化 = 一般阻燃物[灰烬]
+  override def 初始化: 一般阻燃物[灰烬] = new 一般阻燃物(self)
+
 }
 
-object 初始阻燃物 {
-  val value: 初始阻燃物 = new 初始阻燃物
+object 灰烬 {
+  val value: 灰烬 = new 灰烬
 }
