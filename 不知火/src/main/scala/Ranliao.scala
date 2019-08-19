39bff941beb3dccb9479bae2da3a27f1fe0af4f7
djx314 <djx314@sina.cn>
2019-08-19 23:11:34 +0800

    不知火

--- 不知火/src/main/scala/Ranliao.scala
+++ 不知火/src/main/scala/Ranliao.scala
@@ -16,6 +16,18 @@ trait 燃料 {
 
 }
 
+class 燃烧屏障[U <: 燃料](override val 尾: U) extends 燃料 {
+  self =>
+  override val 当前火苗: 零火苗 = 零火苗.零火苗
+  override type 尾    = U
+  override type 当前火苗 = 零火苗
+
+  override type 初始化[T <: 火苗] = 普通燃料[T, 燃烧屏障[U]]
+  override def 初始化[T <: 火苗](t: T): 普通燃料[T, 燃烧屏障[U]] = new 普通燃料(t, self)
+  override type 屏障 = 燃烧屏障[燃烧屏障[U]]
+  override def 屏障: 燃烧屏障[燃烧屏障[U]] = new 燃烧屏障(self)
+}
+
 class 普通燃料[R <: 火苗, U <: 燃料](override val 当前火苗: R, override val 尾: U) extends 燃料 {
   self =>
 
