c9650c404beb9a4d240a6b10264e94d83513ceb6
djx314 <djx314@sina.cn>
2019-08-25 21:46:03 +0800

    新增例子

--- 晒太阳/src/main/scala/Ranliao.scala
+++ 晒太阳/src/main/scala/Ranliao.scala
@@ -11,6 +11,9 @@ trait 燃料 {
   type 初始化[T <: 火苗] <: 燃料
   def 初始化[T <: 火苗](t: T): 初始化[T]
 
+  type flatMap[I <: 燃料] <: 燃料
+  def flatMap[I <: 燃料](i: I): flatMap[I]
+
   type 屏障 <: 燃料
   def 屏障: 屏障
 
@@ -24,6 +27,10 @@ class 燃烧屏障[U <: 燃料](override val 尾: U) extends 燃料 {
 
   override type 初始化[T <: 火苗] = 普通燃料[T, 燃烧屏障[U]]
   override def 初始化[T <: 火苗](t: T): 普通燃料[T, 燃烧屏障[U]] = new 普通燃料(t, self)
+
+  override type flatMap[I <: 燃料] = 尾#flatMap[燃烧屏障[U]#初始化[当前火苗]]
+  override def flatMap[I <: 燃料](i: I): 尾#flatMap[燃烧屏障[U]#初始化[当前火苗]] = 尾.flatMap(self.初始化(零火苗.零火苗))
+
   override type 屏障 = 燃烧屏障[燃烧屏障[U]]
   override def 屏障: 燃烧屏障[燃烧屏障[U]] = new 燃烧屏障(self)
 }
@@ -36,6 +43,9 @@ class 普通燃料[R <: 火苗, U <: 燃料](override val 当前火苗: R, overr
 
   override type 初始化[T <: 火苗] = 普通燃料[T, 普通燃料[R, U]]
   override def 初始化[T <: 火苗](t: T): 普通燃料[T, 普通燃料[R, U]] = new 普通燃料(t, self)
+
+  override type flatMap[I <: 燃料] = 尾#flatMap[I#初始化[当前火苗]]
+  override def flatMap[I <: 燃料](i: I): 尾#flatMap[I#初始化[当前火苗]] = 尾.flatMap(i.初始化(当前火苗))
 
   override type 屏障 = 燃烧屏障[普通燃料[R, U]]
   override def 屏障: 燃烧屏障[普通燃料[R, U]] = new 燃烧屏障(self)
@@ -53,6 +63,9 @@ class 零燃料 extends 燃料 {
 
   override type 初始化[T <: 火苗] = 普通燃料[T, 零燃料]
   override def 初始化[T <: 火苗](t: T): 普通燃料[T, 零燃料] = new 普通燃料(t, self)
+
+  override type flatMap[I <: 燃料] = I
+  override def flatMap[I <: 燃料](i: I): I = i
 
   override type 屏障 = 燃烧屏障[零燃料]
   override def 屏障: 燃烧屏障[零燃料] = new 燃烧屏障(self)
--- 晒太阳/src/main/scala/Yuan.scala
+++ 晒太阳/src/main/scala/Yuan.scala
@@ -2,39 +2,39 @@ import scala.language.higherKinds
 
 trait 缘 {
 
-  type 当前火苗 <: 火苗
-  def 当前火苗: 当前火苗
+  type 当前燃料 <: 燃料
+  def 当前燃料: 当前燃料
 
   type 尾 <: 缘
   def 尾: 尾
 
-  type 初始化[X <: 火苗] <: 缘
-  def 初始化[X <: 火苗](x: X): 初始化[X]
+  type 初始化[X <: 燃料] <: 缘
+  def 初始化[X <: 燃料](x: X): 初始化[X]
 
 }
 
-class 叠加缘分[T <: 火苗, I <: 缘](override val 当前火苗: T, override val 尾: I) extends 缘 {
+class 叠加缘分[T <: 燃料, I <: 缘](override val 当前燃料: T, override val 尾: I) extends 缘 {
   self =>
 
-  override type 当前火苗 = T
+  override type 当前燃料 = T
   override type 尾    = I
 
-  override type 初始化[X <: 火苗] = 叠加缘分[X, 叠加缘分[T, I]]
-  override def 初始化[X <: 火苗](x: X): 叠加缘分[X, 叠加缘分[T, I]] = new 叠加缘分(x, self)
+  override type 初始化[X <: 燃料] = 叠加缘分[X, 叠加缘分[T, I]]
+  override def 初始化[X <: 燃料](x: X): 叠加缘分[X, 叠加缘分[T, I]] = new 叠加缘分(x, self)
 
 }
 
 class 缘尽 extends 缘 {
   self =>
 
-  override type 当前火苗 = 零火苗
-  override def 当前火苗: 零火苗 = 零火苗.零火苗
+  override type 当前燃料 = 零燃料
+  override def 当前燃料: 零燃料 = 零燃料.零燃料
 
   override type 尾 = 缘尽
   override def 尾: 缘尽 = self
 
-  override type 初始化[X <: 火苗] = 叠加缘分[X, 缘尽]
-  override def 初始化[X <: 火苗](x: X): 叠加缘分[X, 缘尽] = new 叠加缘分(x, self)
+  override type 初始化[X <: 燃料] = 叠加缘分[X, 缘尽]
+  override def 初始化[X <: 燃料](x: X): 叠加缘分[X, 缘尽] = new 叠加缘分(x, self)
 
 }
 
