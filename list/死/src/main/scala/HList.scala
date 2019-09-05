b774d9e937bfbbeb6c9cebe25fdef051bcc5d6d1
djx314 <djx314@sina.cn>
2019-09-05 09:40:12 +0800

    死。

--- list/死/src/main/scala/HList.scala
+++ list/死/src/main/scala/HList.scala
@@ -30,18 +30,18 @@ trait HList {
 
 trait PutAble {
 
-  type H <: HList
-  type T
+  type Head
+  type Tail <: HList
 
-  def head: H
+  def head: Head
 
-  def current: T
+  def tail: Tail
 
 }
 
-case class PutAbleImpl[Head <: HList, Tail](override val head: Head, override val current: Tail) extends PutAble {
-  override type H = Head
-  override type T = Tail
+case class PutAbleImpl[H, T <: HList](override val head: H, override val tail: T) extends PutAble {
+  override type Head = H
+  override type Tail = T
 }
 
 class HNil extends HList {
@@ -54,13 +54,13 @@ class HNil extends HList {
 
   override def head: Undefined = Undefined.undefined
 
-  override type ::[R] = AppendAble[HNil, R]
-  override type M[R] = PutAbleImpl[HNil, R]
+  override type ::[R]           = AppendAble[R, HNil]
+  override type M[R]            = PutAbleImpl[R, HNil]
   override type :::[R <: HList] = R
 
-  override def ::[R](xyy: R): AppendAble[HNil, R] = new AppendAble(xyy, self)
+  override def ::[R](xyy: R): AppendAble[R, HNil] = new AppendAble(xyy, self)
 
-  override def put[R](xyy: R): PutAbleImpl[HNil, R] = PutAbleImpl[HNil, R](self, xyy)
+  override def put[R](xyy: R): PutAbleImpl[R, HNil] = PutAbleImpl[R, HNil](xyy, self)
 
   override def :::[R <: HList](xyy: R): R = xyy
 
@@ -82,50 +82,51 @@ object Undefined {
   val undefined: Undefined = Undefined()
 }
 
-class AppendAble[T1 <: HList, T2](override val head: T2, override val tail: T1) extends HList {
+class AppendAble[T1, T2 <: HList](override val head: T1, override val tail: T2) extends HList {
   self =>
 
-  override type Head = T2
-  override type Tail = T1
+  override type Head = T1
+  override type Tail = T2
 
-  override type ::[R] = AppendAble[AppendAble[T1, T2], R]
+  override type ::[R] = AppendAble[R, AppendAble[T1, T2]]
   override type M[R] = ({
-    type MM = T1#M[R]
-    type NN = MM#H#`::`[MM#T]
-    type PP = PutAbleImpl[NN, T2]
+    type MM = T2#M[R]
+    type NN = AppendAble[MM#Head, MM#Tail]
+    type PP = PutAbleImpl[T1, NN]
   })#PP
   override type :::[R <: HList] = ({
-    type MM = R#M[T2]
-    type NN = T1#`:::`[MM#H]#`::`[MM#T]
+    type MM = R#M[T1]
+    type NN = AppendAble[MM#Head, T2# `:::`[MM#Tail]]
   })#NN
 
   override type apply[F <: Fetch] = F#fetch[AppendAble[T1, T2]]#Head
 
   override def apply[F <: Fetch](f: F): F#fetch[AppendAble[T1, T2]]#Head = f.fetch(self).head
 
-  override def ::[R](xyy: R): AppendAble[AppendAble[T1, T2], R] = new AppendAble(xyy, self)
+  override def ::[R](xyy: R): AppendAble[R, AppendAble[T1, T2]] = new AppendAble(xyy, self)
 
   override def put[R](xyy: R): ({
-    type MM = T1#M[R]
-    type NN = MM#H#`::`[MM#T]
-    type PP = PutAbleImpl[NN, T2]
+    type MM = T2#M[R]
+    type NN = AppendAble[MM#Head, MM#Tail]
+    type PP = PutAbleImpl[T1, NN]
   })#PP = {
-    type MM = T1#M[R]
-    type NN = MM#H#`::`[MM#T]
+    type MM = T2#M[R]
+    type NN = AppendAble[MM#Head, MM#Tail]
 
     val mm: MM = tail.put(xyy)
-    val nn: NN = mm.head.::(mm.current: MM#T)
-    PutAbleImpl(nn, head)
+    val nn: NN = new AppendAble(mm.head, mm.tail)
+    PutAbleImpl(head, nn)
   }
 
   override def :::[R <: HList](xyy: R): ({
-    type MM = R#M[T2]
-    type NN = T1#`:::`[MM#H]#`::`[MM#T]
+    type MM = R#M[T1]
+    type NN = AppendAble[MM#Head, T2# `:::`[MM#Tail]]
   })#NN = {
-    type MM = R#M[T2]
+    type MM = R#M[T1]
 
     val mm: MM = xyy.put(head)
-    tail.:::(mm.head: MM#H).::(mm.current: MM#T)
+    //tail.:::(mm.head: MM#H).::(mm.tail: MM#T)
+    new AppendAble(mm.head: MM#Head, tail.:::(mm.tail: MM#Tail))
   }
 
   override def toString: String = s"${head}, ${tail}"
@@ -134,9 +135,9 @@ class AppendAble[T1 <: HList, T2](override val head: T2, override val tail: T1) 
 
 object Miaomiao {
 
-  val i1: Int = 1
+  val i1: Int    = 1
   val i2: String = "miaomiaomiao"
-  val i3: Long = 3L
+  val i3: Long   = 3L
   val i4: Double = 4
 
   /*val mi = (i4 :: HNil.hNil) ::: (i3 :: i2 :: HNil.hNil) ::: (HNil.hNil ::: HNil.hNil) ::: (i1 :: HNil.hNil)
