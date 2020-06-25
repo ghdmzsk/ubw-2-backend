9116f02bf46ff0029f46dcba26d0f031f272c796
djx314 <djx314@sina.cn>
2020-06-25 19:44:44 +0800

    一些优化。

---
+++ a03-十进制后继/a03-core/src/main/scala/Check.scala
@@ -0,0 +1,6 @@
+package shijinzhi
+
+object Check {
+  val aa: Point#_1#_4#_2#_3#Reverse             = (new Point)._3._2._4._1
+  val bb: Point#_1#_4#_2#_0#_0#_3#_0#_0#Reverse = (new Point)._0._0._3._0._0._2._4._1
+}
--- a03-十进制后继/a03-core/src/main/scala/Shijinzhi.scala
+++ a03-十进制后继/a03-core/src/main/scala/Shijinzhi.scala
@@ -5,5 +5,5 @@ class Shijinzhi[T <: JinzhiItem, N <: Jinzhi](val item: T, val count: N) {
 }
 
 object Shijinzhi {
-  val zero: Shijinzhi[JinzhiZero, Leaf_1] = new Shijinzhi(item = new JinzhiZero, count = new Leaf_1)
+  val zero: Shijinzhi[JinzhiZero, Node_1[Point]] = new Shijinzhi(item = new JinzhiZero, count = new Node_1(new Point))
 }
