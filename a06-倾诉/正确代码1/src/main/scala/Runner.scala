48dad4ce2366b20332ab7a0f4292503f7b5759ed
djx314 <djx314@sina.cn>
2020-07-28 17:24:53 +0800

    减法。

--- a06-倾诉/正确代码1/src/main/scala/Runner.scala
+++ a06-倾诉/正确代码1/src/main/scala/Runner.scala
@@ -35,37 +35,9 @@ object Runner {
 
     type HList7_1 = HList3#真的加[HList4]
     type HList7_2 = 零#后继[Test1]#后继[Test2]#后继[Test3]#后继[Test4]#后继[Test5]#后继[Test6]#后继[Test7]
-    hlist3.真的加(hlist4): HList7_1
-    hlist3.真的加(hlist4): HList7_2
-
-    type 负5 = 零#成对后继[Test1]#成对后继[Test2]#成对后继[Test3]#成对后继[Test4]#成对后继[Test5]
-    val 负5: 负5 = 零.value.成对后继(new Test1).成对后继(new Test2).成对后继(new Test3).成对后继(new Test4).成对后继(new Test5)
-
-    type 结果 = 零#后继[Test6]#后继[Test7]
-    val 结果: 结果 = 零.value.后继(new Test6).后继(new Test7)
-
     val hlist7_1: HList7_1 = hlist3.真的加(hlist4)
     val hlist7_2: HList7_2 = hlist7_1
-
-    val 结果1: 结果 = 负5.真的加(hlist7_1)
-    println(结果1) // 零 :: Test6 :: Test7
-
-    val 结果2: 结果 = 负5.真的加(hlist7_2)
-
-    type 结果1 = 负5#真的加[HList7_1]
-    type 结果2 = 负5#真的加[HList7_2]
-    val 结果1_1: 结果1 = 结果
-    val 结果1_2: 结果2 = 结果
-
-    type 结果3 = 零#成对后继[Test1]#成对后继[Test2]
-    val 结果3: 结果3 = 负5.真的加(hlist3)
-    println(结果3) // 零 反向 Test1 反向 Test2
-
-    type 负3 = 零#成对后继[Test1]#成对后继[Test2]#成对后继[Test3]
-    val 负3: 负3 = 零.value.成对后继(new Test1).成对后继(new Test2).成对后继(new Test3)
-    println(hlist7_1.真的加(负5))
-    println(hlist7_1.真的加(负3))
-    println(负5.真的加(负5))
+    println(hlist7_2) // 零 :: Test1 :: Test2 :: Test3 :: Test4 :: Test5 :: Test6 :: Test7
   }
 
 }
