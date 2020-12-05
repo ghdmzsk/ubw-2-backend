0c593287defbf562c15130900f79fe7abc7336db
djx314 <djx314@sina.cn>
2020-12-05 12:28:35 +0800

    rwerwers

--- a20-月盈月亏/src/main/scala/a20/step2_2020120501/Nat.scala
+++ a20-月盈月亏/src/main/scala/a20/step2_2020120501/Nat.scala
@@ -1,12 +1,24 @@
 package a20.step2_2020120501
 
 trait Nat {
-  type M[I <: Nat] <: Moon
+  type M[I <: Nat, C] <: Moon
+}
+
+class NatZero extends Nat {
+  override type M[I <: Nat, C] = MoonImpl[I, NatZero, C]
+}
+
+class NatPositive[Tail <: Nat, Head] extends Nat {
+  override type M[I <: Nat, C] = MoonImpl[Tail, NatPositive[I, C], Head]
 }
 
 trait Moon {
   type Current
+  type Next <: Moon
+  type Apply[N1 <: Nat, C] = MoonImpl[N1, NatZero, C]
 }
+
 class MoonImpl[N1 <: Nat, N2 <: Nat, C] extends Moon {
   override type Current = C
+  override type Next    = N1#M[N2, C]
 }
---
+++ a20-月盈月亏/src/main/scala/a20/step2_2020120501/测试.scala
@@ -0,0 +1,40 @@
+package a20.step2_2020120501
+
+object 测试 {
+  class 新月
+  class 上弦
+  class 渐盈
+  class 满月
+
+  class 匹配[T]
+
+  object 匹配 {
+    def apply[T]: 匹配[T]                          = new 匹配[T]
+    def 匹配[T](i1: 匹配[T], i2: 匹配[T]): List[匹配[T]] = List(i1, i2)
+  }
+
+  type Moon1 = Moon#Apply[NatPositive[NatPositive[NatPositive[NatZero, 满月], 渐盈], 上弦], 新月]
+
+  匹配.匹配(匹配[新月], 匹配[Moon1#Current])
+  匹配.匹配(匹配[上弦], 匹配[Moon1#Next#Current])
+  匹配.匹配(匹配[渐盈], 匹配[Moon1#Next#Next#Current])
+  匹配.匹配(匹配[满月], 匹配[Moon1#Next#Next#Next#Current])
+  匹配.匹配(匹配[满月], 匹配[Moon1#Next#Next#Next#Next#Current])
+  匹配.匹配(匹配[渐盈], 匹配[Moon1#Next#Next#Next#Next#Next#Current])
+  匹配.匹配(匹配[上弦], 匹配[Moon1#Next#Next#Next#Next#Next#Next#Current])
+  匹配.匹配(匹配[新月], 匹配[Moon1#Next#Next#Next#Next#Next#Next#Next#Current])
+  匹配.匹配(匹配[新月], 匹配[Moon1#Next#Next#Next#Next#Next#Next#Next#Next#Current])
+  匹配.匹配(匹配[上弦], 匹配[Moon1#Next#Next#Next#Next#Next#Next#Next#Next#Next#Current])
+
+  type Moon2 = Moon1#Next#Next#Next#Next#Next#Next#Next#Next#Next#Next
+  匹配.匹配(匹配[渐盈], 匹配[Moon2#Current])
+  匹配.匹配(匹配[满月], 匹配[Moon2#Next#Current])
+  匹配.匹配(匹配[满月], 匹配[Moon2#Next#Next#Current])
+  匹配.匹配(匹配[渐盈], 匹配[Moon2#Next#Next#Next#Current])
+  匹配.匹配(匹配[上弦], 匹配[Moon2#Next#Next#Next#Next#Current])
+  匹配.匹配(匹配[新月], 匹配[Moon2#Next#Next#Next#Next#Next#Current])
+  匹配.匹配(匹配[新月], 匹配[Moon2#Next#Next#Next#Next#Next#Next#Current])
+  匹配.匹配(匹配[上弦], 匹配[Moon2#Next#Next#Next#Next#Next#Next#Next#Current])
+  匹配.匹配(匹配[渐盈], 匹配[Moon2#Next#Next#Next#Next#Next#Next#Next#Next#Current])
+
+}
