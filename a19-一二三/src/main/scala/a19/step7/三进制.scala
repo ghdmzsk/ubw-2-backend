562fbcc224f1ff70f0f7efb9d29b98748ba4769b
djx314 <djx314@sina.cn>
2020-12-15 23:09:42 +0800

    sfwer

---
+++ a19-一二三/src/main/scala/a19/step7/三进制.scala
@@ -0,0 +1,196 @@
+package a19.step7
+
+trait 进位准备 {
+  type Next[T] <: 进位准备
+  type 加数后上级进位操作[NoNeed <: 进位准备, Need <: 进位准备] <: 进位准备
+}
+
+/*trait 来去 {
+  type Num[N] <: 进位准备
+  type 进位[N <: 进位准备, H <: 本阶变化] <: 来去
+  type 不进位[N <: 进位准备, H <: 本阶变化] <: 来去
+}
+
+class 初始进位来去[Num1 <: 进位准备] extends 来去 {
+  override type Num            = Num1
+  override type 进位[H <: 本阶变化]  = H#进位变化[Num1]
+  override type 不进位[H <: 本阶变化] = H#不进位变化[Num1]
+}
+
+class 初始不进位来去[Num1 <: 进位准备] extends 来去 {
+  override type Num            = Num1
+  override type 进位[H <: 本阶变化]  = H#进位变化[Num1]
+  override type 不进位[H <: 本阶变化] = H#不进位变化[Num1]
+}
+
+class 复杂进位来去[Tail <: 来去] extends 来去 {
+  override type Num            = Tail#Num
+  override type 进位[H <: 本阶变化]  = Tail#进位[H]
+  override type 不进位[H <: 本阶变化] = Tail#不进位[H]
+}
+
+class 复杂不进位来去[Tail <: 来去] extends 来去 {
+  override type 进位[H <: 本阶变化]  = Tail#不进位[H]
+  override type 不进位[H <: 本阶变化] = Tail#不进位[H]
+}*/
+
+trait 变化 {
+  type 转化[N <: 进位准备] <: 进位准备
+}
+
+trait 初始转换 {
+  type 小转换[T] <: 进位准备
+}
+
+trait 本阶变化 {
+  type 变换[N] <: 进位准备
+  type 进位[进1 <: 变化, 不进1 <: 变化] <: 本阶变化
+  type 不进位[进1 <: 变化, 不进1 <: 变化] <: 本阶变化
+}
+
+class 本阶变化零[N1 <: 初始转换] extends 本阶变化 {
+  override type 变换[N]                    = N1#小转换[N1]
+  override type 进位[进1 <: 变化, 不进1 <: 变化]  = 本阶变化进位[本阶变化零[N1], 进1, 不进1]
+  override type 不进位[进1 <: 变化, 不进1 <: 变化] = 本阶变化不进位[本阶变化零[N1], 进1, 不进1]
+}
+
+class 本阶变化进位[Tail <: 本阶变化, 进 <: 变化, 不进 <: 变化] extends 本阶变化 {
+  override type 变换[N]                    = 进#转化[Tail#变换[N]]
+  override type 进位[进1 <: 变化, 不进1 <: 变化]  = 本阶变化进位[本阶变化进位[Tail, 进, 不进], 进1, 不进1]
+  override type 不进位[进1 <: 变化, 不进1 <: 变化] = 本阶变化不进位[本阶变化进位[Tail, 进, 不进], 进1, 不进1]
+}
+
+class 本阶变化不进位[Tail <: 本阶变化, 进 <: 变化, 不进 <: 变化] extends 本阶变化 {
+  override type 变换[N]                    = 不进#转化[Tail#变换[不进]]
+  override type 进位[进1 <: 变化, 不进1 <: 变化]  = 本阶变化不进位[本阶变化不进位[Tail, 进, 不进], 进1, 不进1]
+  override type 不进位[进1 <: 变化, 不进1 <: 变化] = 本阶变化不进位[本阶变化不进位[Tail, 进, 不进], 进1, 不进1]
+}
+
+class P_3_Step_1[T1, T2, II1 <: 进位准备, II2 <: 进位准备] extends 进位准备 {
+  type _1_th = T1
+  type _2_th = T2
+
+  type _1 = II1
+  type _2 = II2
+
+  override type Next[T] = II1#加数后上级进位操作[P_3_Step_1[T1, T2, II1#Next[T], II2], P_3_Step_2[T1, T2, II1#Next[T], II2]]
+}
+
+class P_3_Step_2[T1, T2, II1 <: 进位准备, II2 <: 进位准备] extends 进位准备 {
+  type _1_th = T1
+  type _2_th = T2
+
+  type _1 = II1
+  type _2 = II2
+
+  override type Next[T] = II2#加数后上级进位操作[P_3_Step_2[T1, T2, II1, II2#Next[T]], P_3_Step_1[T1, T2, II1, II2#Next[T]]]
+}
+
+class S_0[T0] extends 进位准备 {
+  type _0_th = T0
+
+  override type Next[T]                             = S_1[T0, T]
+  override type 加数后上级进位操作[II <: 进位准备, Need <: 进位准备] = II
+}
+
+class S_1[T0, T1] extends 进位准备 {
+  type _0_th = T0
+  type _1_th = T1
+
+  override type Next[T]                             = S_2[T0, T1, T]
+  override type 加数后上级进位操作[II <: 进位准备, Need <: 进位准备] = Need
+}
+
+class S_2[T0, T1, T2] extends 进位准备 {
+  type _0_th = T0
+  type _1_th = T1
+  type _2_th = T2
+
+  override type Next[T]                             = I_0[T0, T1, T2, S_0[T]]
+  override type 加数后上级进位操作[II <: 进位准备, Need <: 进位准备] = II
+}
+
+class I_0[T0, T1, T2, II0 <: 进位准备] extends 进位准备 {
+  type _0_th = T0
+  type _1_th = T1
+  type _2_th = T2
+
+  type _0 = II0
+
+  override type Next[T]                             = II0#加数后上级进位操作[I_0[T0, T1, T2, II0#Next[T]], I_0_To_1[T0, T1, T2, II0#Next[T]]]
+  override type 加数后上级进位操作[II <: 进位准备, Need <: 进位准备] = II
+}
+
+class I_0_To_1[T0, T1, T2, II0 <: 进位准备] extends 进位准备 {
+  type _0_th = T0
+  type _1_th = T1
+  type _2_th = T2
+
+  type _0 = II0
+
+  override type Next[T]                             = I_1[T0, T1, T2, II0, S_0[T]]
+  override type 加数后上级进位操作[II <: 进位准备, Need <: 进位准备] = II
+}
+
+class I_1[T0, T1, T2, II0 <: 进位准备, II1 <: 进位准备] extends 进位准备 {
+  type _0_th = T0
+  type _1_th = T1
+  type _2_th = T2
+
+  type _0 = II0
+  type _1 = II1
+
+  override type Next[T]                             = II1#加数后上级进位操作[I_1[T0, T1, T2, II0, II1#Next[T]], I_1_To_2[T0, T1, T2, II0, II1#Next[T]]]
+  override type 加数后上级进位操作[II <: 进位准备, Need <: 进位准备] = II
+}
+
+class I_1_To_2[T0, T1, T2, II0 <: 进位准备, II1 <: 进位准备] extends 进位准备 {
+  type _0_th = T0
+  type _1_th = T1
+  type _2_th = T2
+
+  type _0 = II0
+  type _1 = II1
+
+  override type Next[T]                             = I_2_Step_2[T0, T1, T2, II0, II1, S_0[T]]
+  override type 加数后上级进位操作[II <: 进位准备, Need <: 进位准备] = II
+}
+
+class I_2_Step_0[T0, T1, T2, II0 <: 进位准备, II1 <: 进位准备, II2 <: 进位准备] extends 进位准备 {
+  type _0_th = T0
+  type _1_th = T1
+  type _2_th = T2
+
+  type _0 = II0
+  type _1 = II1
+  type _2 = II2
+
+  override type Next[T]                             = II0#加数后上级进位操作[I_2_Step_0[T0, T1, T2, II0#Next[T], II1, II2], I_2_Step_1[T0, T1, T2, II0#Next[T], II1, II2]]
+  override type 加数后上级进位操作[II <: 进位准备, Need <: 进位准备] = II
+}
+
+class I_2_Step_1[T0, T1, T2, II0 <: 进位准备, II1 <: 进位准备, II2 <: 进位准备] extends 进位准备 {
+  type _0_th = T0
+  type _1_th = T1
+  type _2_th = T2
+
+  type _0 = II0
+  type _1 = II1
+  type _2 = II2
+
+  override type Next[T]                             = II1#加数后上级进位操作[I_2_Step_1[T0, T1, T2, II0, II1#Next[T], II2], I_2_Step_2[T0, T1, T2, II0, II1#Next[T], II2]]
+  override type 加数后上级进位操作[II <: 进位准备, Need <: 进位准备] = II
+}
+
+class I_2_Step_2[T0, T1, T2, II0 <: 进位准备, II1 <: 进位准备, II2 <: 进位准备] extends 进位准备 {
+  type _0_th = T0
+  type _1_th = T1
+  type _2_th = T2
+
+  type _0 = II0
+  type _1 = II1
+  type _2 = II2
+
+  override type Next[T]                             = II2#加数后上级进位操作[I_2_Step_2[T0, T1, T2, II0, II1, II2#Next[T]], I_2_Step_0[T0, T1, T2, II0, II1, II2#Next[T]]]
+  override type 加数后上级进位操作[II <: 进位准备, Need <: 进位准备] = II2#加数后上级进位操作[II, Need]
+}
---
+++ a19-一二三/src/main/scala/a19/step7/测试.scala
@@ -0,0 +1,132 @@
+package a19.step7
+
+class 类型匹配[T]
+
+object 类型匹配 {
+  def apply[T]                                              = new 类型匹配[T]
+  def apply[T](i: 类型匹配[T], ii: 类型匹配[T]): (类型匹配[T], 类型匹配[T]) = (i, ii)
+}
+
+class Item1
+class Item2
+class Item3
+class Item4
+class Item5
+class Item6
+class Item7
+class Item8
+class Item9
+class Item10
+class Item11
+class Item12
+class Item13
+class Item14
+class Item15
+class Item16
+class Item17
+class Item18
+class Item19
+class Item20
+class Item21
+class Item22
+class Item23
+class Item24
+class Item25
+class Item26
+class Item27
+class Item28
+class Item29
+class Item30
+class Item31
+class Item32
+class Item33
+class Item34
+class Item35
+class Item36
+class Item37
+class Item38
+class Item39
+class Item40
+class Item41
+class Item42
+class Item43
+class Item44
+class Item45
+class Item46
+class Item47
+class Item48
+class Item49
+class Item50
+class Item51
+class Item52
+class Item53
+class Item54
+class Item55
+class Item56
+class Item57
+class Item58
+class Item59
+class Item60
+class Item61
+class Item62
+class Item63
+class Item64
+class Item65
+class Item66
+class Item67
+class Item68
+class Item69
+class Item70
+class Item71
+class Item72
+class Item73
+class Item74
+class Item75
+class Item76
+class Item77
+class Item78
+class Item79
+class Item80
+class Item81
+class Item82
+class Item83
+
+object 测试 {
+
+  type jihjkhjih       = P_3_Step_2[Item1, Item2, S_2[Item3, Item4, Item5], S_0[Item6]]
+  type nlhnjsnroewrnwe = jihjkhjih#Next[Item7]#Next[Item8]#Next[Item9]#Next[Item10]#Next[Item11]
+  type dfgdfgereter    = nlhnjsnroewrnwe#Next[Item12]#Next[Item13]#Next[Item14]#Next[Item15]#Next[Item16]#Next[Item17]
+  type rhgrtrbrbrtbrtg = dfgdfgereter#Next[Item18]#Next[Item19]#Next[Item20]#Next[Item21]#Next[Item22]#Next[Item23]#Next[Item24]#Next[Item25]#Next[Item26]#Next[
+    Item27
+  ]#Next[Item28]#Next[Item29]#Next[Item30]#Next[Item31]#Next[Item32]#Next[Item33]#Next[Item34]#Next[Item35]#Next[Item36]
+
+  type r50 = rhgrtrbrbrtbrtg#Next[Item37]#Next[Item38]#Next[Item39]#Next[Item40]#Next[Item41]#Next[Item42]#Next[Item43]#Next[Item44]#Next[Item45]#Next[Item46]#Next[
+    Item47
+  ]#Next[Item48]#Next[Item49]#Next[Item50]
+
+  type r60 = r50#Next[Item51]#Next[Item52]#Next[Item53]#Next[Item54]#Next[Item55]#Next[Item56]#Next[Item57]#Next[Item58]#Next[Item59]#Next[Item60]
+  type r70 = r60#Next[Item61]#Next[Item62]#Next[Item63]#Next[Item64]#Next[Item65]#Next[Item66]#Next[Item67]#Next[Item68]#Next[Item69]#Next[Item70]
+  type r80 = r70#Next[Item71]#Next[Item72]#Next[Item73]#Next[Item74]#Next[Item75]#Next[Item76]#Next[Item77]#Next[Item78]#Next[Item79]#Next[Item80]
+  type r83 = r80#Next[Item81]#Next[Item82]#Next[Item83]
+
+  类型匹配(类型匹配[nlhnjsnroewrnwe#_2_th], 类型匹配[Item2])
+
+  类型匹配(类型匹配[nlhnjsnroewrnwe#_1#_2_th], 类型匹配[Item5])
+  类型匹配(类型匹配[dfgdfgereter#_2#_1_th], 类型匹配[Item7])
+
+  类型匹配(类型匹配[dfgdfgereter#_1#_2#_1_th], 类型匹配[Item16])
+  类型匹配(类型匹配[rhgrtrbrbrtbrtg#_1#_0#_2#_1_th], 类型匹配[Item34])
+  类型匹配(类型匹配[r50#_1#_2#_1#_2_th], 类型匹配[Item50])
+  类型匹配(类型匹配[r60#_2#_0#_2#_0_th], 类型匹配[Item60])
+  类型匹配(类型匹配[r70#_2#_1#_2#_1_th], 类型匹配[Item70])
+  类型匹配(类型匹配[r80#_2#_2#_2#_2_th], 类型匹配[Item80])
+  类型匹配(类型匹配[r83#_1#_0#_0#_0#_0_th], 类型匹配[Item81])
+  类型匹配(类型匹配[r83#_1#_0#_0#_0#_1_th], 类型匹配[Item82])
+  类型匹配(类型匹配[r83#_1#_0#_0#_0#_2_th], 类型匹配[Item83])
+  类型匹配(类型匹配[r83#_2#_0#_2#_0_th], 类型匹配[Item60])
+  类型匹配(类型匹配[r83#_2#_0#_2#_1_th], 类型匹配[Item61])
+
+  def main(arr: Array[String]): Unit = {
+    println("22")
+  }
+}
