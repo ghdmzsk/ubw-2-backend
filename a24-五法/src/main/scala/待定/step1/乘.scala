520e0d60c1a7c53812af5a6c38994e3df6cf5a80
djx314 <djx314@sina.cn>
2021-01-12 21:18:33 +0800

    nn

---
+++ a24-五法/src/main/scala/待定/step1/乘.scala
@@ -0,0 +1,31 @@
+package 待定.step1
+
+class Item1
+class Item2
+class Item3
+class Item4
+
+trait Ba[T <: Ba[T, H], H] {
+  type Tail <: Ba[Tail, Head]
+  type Head
+}
+
+class A1 extends Ba[A1, Item1] {
+  override type Tail = A2
+  override type Head = Item2
+}
+
+class A2 extends Ba[A2, Item2] {
+  override type Tail = A3
+  override type Head = Item3
+}
+
+class A3 extends Ba[A3, Item3] {
+  override type Tail = A4
+  override type Head = Item4
+}
+
+class A4 extends Ba[A4, Item4] {
+  override type Tail = A1
+  override type Head = Item1
+}
