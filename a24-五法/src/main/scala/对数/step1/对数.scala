ce829cf82a02635d5bba26c880ad66e007bb95a1
djx314 <djx314@sina.cn>
2021-01-15 05:58:00 +0800

    sgasdfhwaeiurhweiuhrwaeiu rbwuhebr awsyheuvtqweyhuvb tqeyuiwryuwgvtb

--- a24-五法/src/main/scala/对数/step1/对数.scala
+++ a24-五法/src/main/scala/对数/step1/对数.scala
@@ -11,7 +11,7 @@ trait 底数Positive[Total <: 底数, Tail <: 底数] extends 底数 {
   override type 被对[I <: 真数, Head, T <: 对数]   = T#对[I, Head, Total]#天道索求[Tail, T]
   override type 深度被对[I <: 真数, Head, T <: 对数] = T#对[I, Head, Total]#深度索求[Tail, T]
   override type 直对花心[I <: 真数, Head]          = I#地道索求[Tail]
-  override type 天道消去[I <: 真数, Head]          = I#对[Tail]
+  override type 天道消去[I <: 真数, Head]          = I#内部对[Tail]
 }
 
 trait 底数One[Tail <: 底数] extends 底数 {
@@ -22,21 +22,22 @@ trait 底数One[Tail <: 底数] extends 底数 {
 }
 
 trait 真数 {
-  type 对[N <: 底数] <: 广义对数
+  type 内部对[N <: 底数] <: 广义对数
   type 地道索求[N <: 底数] <: 真数
   type 深度索求[N <: 底数, T <: 对数] <: 真数
   type 天道索求[N <: 底数, T <: 对数] <: 对数
 }
 
 class 真数Positive[Tail <: 真数, Head] extends 真数 {
-  override type 对[N <: 底数]             = N#天道消去[Tail, Head]
+  type 对[N <: 底数]                      = N#天道消去[Tail, Head]
+  override type 内部对[N <: 底数]           = N#天道消去[Tail, Head]
   override type 地道索求[N <: 底数]          = N#直对花心[Tail, Head]
   override type 深度索求[N <: 底数, T <: 对数] = N#深度被对[Tail, Head, T]
   override type 天道索求[N <: 底数, T <: 对数] = N#被对[Tail, Head, T]
 }
 
 class 真数Zero extends 真数 {
-  override type 对[N <: 底数]             = 对数Zero
+  override type 内部对[N <: 底数]           = 对数Zero
   override type 地道索求[N <: 底数]          = 真数Zero
   override type 深度索求[N <: 底数, T <: 对数] = 真数Zero
   override type 天道索求[N <: 底数, T <: 对数] = T
