1a8aed322312c3c2fd9be786b6be1081b794d38b
djx314 <djx314@sina.cn>
2021-02-13 14:50:24 +0800

    sdfsdfs

---
+++ a31-整数/src/main/scala/step2/整数.scala
@@ -0,0 +1,25 @@
+package step2
+
+trait 整数 {
+  type 加[T <: 整数] <: 整数
+  type 被加正数[T <: 整数, H] <: 整数
+  type 被加负数[T[D] <: 整数, H] <: 整数
+}
+
+class 零 extends 整数 {
+  override type 加[T <: 整数]          = T
+  override type 被加正数[T <: 整数, H]    = 正数[T, H]
+  override type 被加负数[T[D] <: 整数, H] = 负数[T, H]
+}
+
+class 正数[Tail <: 整数, Head] extends 整数 {
+  override type 加[T <: 整数]          = T#被加正数[Tail, Head]
+  override type 被加正数[T <: 整数, H]    = 正数[Tail#被加正数[T, H], Head]
+  override type 被加负数[T[D] <: 整数, H] = T[Head]#加[Tail]
+}
+
+class 负数[Tail[D] <: 整数, Head] extends 整数 {
+  override type 加[T <: 整数]          = T#被加负数[Tail, Head]
+  override type 被加正数[T <: 整数, H]    = T#加[Tail[H]]
+  override type 被加负数[T[D] <: 整数, H] = 负数[({ type N[M] = Tail[M]#被加负数[T, H] })#N, Head]
+}
