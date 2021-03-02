d3b9d1c3f4a61d04159e69ce6909f34241eda2fe
djx314 <djx314@sina.cn>
2021-03-02 12:46:53 +0800

    sfdsf

--- a33-对数/src/main/scala/step1/Runner.scala
+++ a33-对数/src/main/scala/step1/Runner.scala
@@ -1,6 +1,6 @@
 package step1
 
-object Runner {
+object Runner1 {
 
   class Tag[T]
 
@@ -14,20 +14,64 @@ object Runner {
   class Item6
   class Item7
   class Item8
+  class Item9
+  class Item10
+  class Item11
+  class Item12
+  class Item13
+  class Item14
+  class Item15
+  class Item16
+  class Item17
 
-  /*type 被减数1 = 被减数Positive[被减数Positive[被减数Positive[被减数Zero, Item1], Item2], Item3]
-  type 减数1  = 减数Positive[减数Positive[减数Positive[减数Positive[减数Positive[减数Zero, Item4], Item5], Item6], Item7], Item8]
-  type 结果1  = 减数Positive[减数Positive[减数Zero, Item4], Item5]
-  i(new Tag[结果1], new Tag[减数1#减[被减数1]])
+  class 底数2 extends 底Positive[底数1, Item17]
+  class 底数1 extends 底1[底数2]
 
-  type 被减数2 = 被减数Positive[被减数Positive[被减数Positive[被减数Positive[被减数Zero, Item1], Item2], Item3], Item4]
-  type 减数2  = 减数Positive[减数Zero, Item5]
-  type 结果2  = 被减数Positive[被减数Positive[被减数Positive[被减数Zero, Item1], Item2], Item3]
-  i(new Tag[结果2], new Tag[减数2#减[被减数2]])
+  type 真数1 = 真数Positive[真数Zero, Item1]
+  i(new Tag[log#run[底数2, 真数1]], new Tag[对数Zero])
 
-  type 被减数3 = 被减数Positive[被减数Positive[被减数Positive[被减数Zero, Item1], Item2], Item3]
-  type 减数3  = 减数Positive[减数Positive[减数Positive[减数Zero, Item4], Item5], Item6]
-  type 结果3  = 被减数Zero*/
-  // i(new Tag[结果3], new Tag[减数3#减[被减数3]])
+  type 真数2 = 真数Positive[真数1, Item2]
+  i(new Tag[log#run[底数2, 真数2]], new Tag[对数Positive[对数Zero]])
 
+  type 真数3 = 真数Positive[真数2, Item3]
+  i(new Tag[log#run[底数2, 真数3]], new Tag[对数Positive[对数Zero]])
+
+  type 真数4 = 真数Positive[真数3, Item4]
+  i(new Tag[log#run[底数2, 真数4]], new Tag[对数Positive[对数Zero]])
+
+  /*type 真数5 = 真数Positive[真数4, Item5]
+  i(new Tag[log#run[底数2, 真数5]], new Tag[对数Positive[对数Zero]])
+
+  type 真数6 = 真数Positive[真数5, Item6]
+  i(new Tag[log#run[底数2, 真数6]], new Tag[对数Positive[对数Positive[对数Zero]]])
+
+  type 真数7 = 真数Positive[真数6, Item7]
+  i(new Tag[log#run[底数2, 真数7]], new Tag[对数Positive[对数Positive[对数Zero]]])
+
+  type 真数8 = 真数Positive[真数7, Item8]
+  i(new Tag[log#run[底数2, 真数8]], new Tag[对数Positive[对数Positive[对数Zero]]])
+
+  type 真数9 = 真数Positive[真数8, Item9]
+  i(new Tag[log#run[底数2, 真数9]], new Tag[对数Positive[对数Positive[对数Zero]]])
+
+  type 真数10 = 真数Positive[真数9, Item10]
+  i(new Tag[log#run[底数2, 真数10]], new Tag[对数Positive[对数Positive[对数Zero]]])
+
+  type 真数11 = 真数Positive[真数10, Item11]
+  i(new Tag[log#run[底数2, 真数11]], new Tag[对数Positive[对数Positive[对数Zero]]])
+
+  type 真数12 = 真数Positive[真数11, Item12]
+  i(new Tag[log#run[底数2, 真数12]], new Tag[对数Positive[对数Positive[对数Positive[对数Zero]]]])
+
+  type 真数13 = 真数Positive[真数12, Item13]
+  i(new Tag[log#run[底数2, 真数13]], new Tag[对数Positive[对数Positive[对数Positive[对数Zero]]]])
+
+  type 真数14 = 真数Positive[真数13, Item14]
+  i(new Tag[log#run[底数2, 真数14]], new Tag[对数Positive[对数Positive[对数Positive[对数Zero]]]])
+
+  type 真数15 = 真数Positive[真数14, Item15]
+  i(new Tag[log#run[底数2, 真数15]], new Tag[对数Positive[对数Positive[对数Positive[对数Zero]]]])
+
+  type 真数16 = 真数Positive[真数15, Item16]
+  i(new Tag[log#run[底数2, 真数16]], new Tag[对数Positive[对数Positive[对数Positive[对数Zero]]]])*/
 }
--- a33-对数/src/main/scala/step1/对数.scala
+++ a33-对数/src/main/scala/step1/对数.scala
@@ -1,32 +1,37 @@
 package step1
 
 trait 底 {
-  type 深入[T <: 真数, R <: 真数] <: 对数
-  type 划圈[T <: 真数, R <: 真数] <: 对数
+  type 划圈[T <: 真数, H, R1 <: 隐数, R2 <: 隐数] <: 对数
 }
 class 底Positive[Tail <: 底, Head] extends 底 {
-  override type 深入[T <: 真数, R <: 真数] = T#对[Tail, Head]
-  override type 划圈[T <: 真数, R <: 真数] = T#被划圈[Tail, Head, R]
+  override type 划圈[T <: 真数, H, R1 <: 隐数, R2 <: 隐数] = T#被划圈[Tail, 隐数Positive[R1], R2]
 }
-class 底Zero[Tail <: 底] extends 底 {
-  override type 深入[T <: 真数, R <: 真数] = 对数Positive[Tail#划圈[T, R]]
-  override type 划圈[T <: 真数, R <: 真数] = R#G点[Tail, T]
+class 底1[Tail <: 底] extends 底 {
+  override type 划圈[T <: 真数, H, R1 <: 隐数, R2 <: 隐数] = R2#G点[Tail, T, 隐数Positive[R1]]
 }
 
 trait 真数 {
-  type 对[D <: 底, H] <: 对数
-  type 被划圈[D <: 底, H, R <: 真数] <: 对数
-  type G点[D <: 底, T <: 真数] <: 对数
+  type 被划圈[D <: 底, R1 <: 隐数, R2 <: 隐数] <: 对数
 }
-class 真数Positive[Tail <: 真数, Reverse <: 真数, Head] extends 真数 {
-  override type 对[D <: 底, H]            = D#深入[Tail, Reverse]
-  override type 被划圈[D <: 底, H, R <: 真数] = D#划圈[Tail, R]
-  override type G点[D <: 底, T <: 真数]     = D#划圈[T, Tail]
+class 真数Positive[Tail <: 真数, Head] extends 真数 {
+  override type 被划圈[D <: 底, R1 <: 隐数, R2 <: 隐数] = D#划圈[Tail, Head, R1, R2]
 }
-class 真数Zero[Reverse <: 真数] extends 真数 {
-  override type 对[D <: 底, H]            = 对数Zero
-  override type 被划圈[D <: 底, H, R <: 真数] = 对数Zero
-  override type G点[D <: 底, T <: 真数]     = 对数Positive[D#深入[T]]
+class 真数Zero extends 真数 {
+  override type 被划圈[D <: 底, R1 <: 隐数, R2 <: 隐数] = 对数Zero
+}
+
+trait 隐数 {
+  type G点[D <: 底, T <: 真数, R <: 隐数] <: 对数
+}
+class 隐数Positive[Tail <: 隐数] extends 隐数 {
+  override type G点[D <: 底, T <: 真数, R <: 隐数] = T#被划圈[D, R, Tail]
+}
+class 隐数Zero extends 隐数 {
+  override type G点[D <: 底, T <: 真数, R <: 隐数] = 对数Positive[T#被划圈[D, 隐数Zero, R]]
+}
+
+class log {
+  type run[D <: 底, T <: 真数] = T#被划圈[D, 隐数Zero, 隐数Zero]
 }
 
 trait 对数
