fa1ff8735275dfd3ac4b33245521aeb2b313aabf
djx314 <djx314@sina.cn>
2021-03-18 10:17:22 +0800

    sfsfwer

---
+++ a28-练习/src/main/scala/练习19/重组.scala
@@ -0,0 +1,30 @@
+package 练习19
+
+object 重组 {
+
+  trait HList
+  class HPositive[Head, Tail <: HList] extends HList
+  class HZero                          extends HList
+
+  class 五十里好好的
+  class 五十里准备中弹
+  class 五十里中弹
+  class 五十里流血
+  class 五十里死亡准备
+
+  type 后续事件 = HList
+
+  type 五十里1 = HPositive[五十里好好的, HPositive[五十里准备中弹, HPositive[五十里中弹, HPositive[五十里流血, HPositive[五十里死亡准备, 后续事件]]]]]
+
+  class 情报体历史 extends HPositive[五十里中弹, HPositive[五十里流血, HPositive[五十里死亡准备, 重组]]]
+  class 重组    extends HPositive[五十里准备中弹, 情报体历史]
+
+  type 改写后的五十里 = HPositive[五十里好好的, 情报体历史]
+
+  println("世界：你个死 lazy，你有完没完，呕你出来")
+
+  type 世界修正力发动后的五十里 = HPositive[五十里好好的, 后续事件]
+
+  println("情报体历史被世界抛弃，术式消散")
+
+}
