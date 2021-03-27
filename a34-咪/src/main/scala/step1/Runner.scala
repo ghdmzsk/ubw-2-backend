641177b7324b9295c6067e0778704cc531b74d19
djx314 <djx314@sina.cn>
2021-03-27 22:28:15 +0800

    fwetwerwedwedwe

--- a34-咪/src/main/scala/step1/Runner.scala
+++ a34-咪/src/main/scala/step1/Runner.scala
@@ -7,13 +7,15 @@ object Runner {
   def i[T](tag1: Tag[T], tag2: Tag[T]): List[Tag[T]] = List(tag1, tag2)
 
   def main(arr: Array[String]): Unit = {
-    val 指数10: 指数 = new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数1)))))))))
-    val 咪1       = 指数10.指(new 底数3_1)
+    val 指数10: 指数 = new 指数Positive(
+      new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Positive(new 指数Zero)))))))))
+    )
+    val 咪1 = 指数10.指(new 底数1)
     println(scala.math.pow(3, 10)) // 59049.0
     println(咪1.length)             // 咪(59049)
 
-    val 指数3 = new 指数Positive(new 指数Positive(new 指数1))
-    val 咪2  = 指数3.指(new 底数3_1)
+    val 指数3: 指数 = new 指数Positive(new 指数Positive(new 指数Positive(new 指数Zero)))
+    val 咪2      = 指数3.指(new 底数1)
     println(咪2.length) // 咪(27)
   }
 
--- a34-咪/src/main/scala/step1/咪咪咪.scala
+++ a34-咪/src/main/scala/step1/咪咪咪.scala
@@ -4,8 +4,8 @@ trait 底数 {
   def length: 咪
 }
 
-class 底数3_1 extends 底数 {
-  override val length: 咪 = 咪(3)
+class 底数1 extends 底数 {
+  override val length: 咪 = 咪(1)
 }
 class 底数3_Positive[I1 <: 底数, I2 <: 底数, I3 <: 底数](i1: I1, i2: I2, i3: I3) extends 底数 {
   override def length: 咪 = i1.length + i2.length + i3.length
@@ -19,7 +19,7 @@ class 指数Positive[Tail <: 指数](tail: Tail) extends 指数 {
   override type 指[I <: 底数] = Tail#指[底数3_Positive[I, I, I]]
   override def 指[I <: 底数](i: I): Tail#指[底数3_Positive[I, I, I]] = tail.指(new 底数3_Positive(i, i, i))
 }
-class 指数1 extends 指数 {
+class 指数Zero extends 指数 {
   override type 指[I <: 底数] = I
   override def 指[I <: 底数](i: I): I = i
 }
