5c64c4090a3b7b51a34fd826534cd987e1142403
djx271 <2066473905@qq.com>
2021-04-15 04:57:09 +0800

    sdfsdf

--- a28-练习/src/main/scala/练习22/Runner.scala
+++ a28-练习/src/main/scala/练习22/Runner.scala
@@ -3,6 +3,8 @@ package 练习22
 object Runner {
 
   def main(arr: Array[String]): Unit = {
+    // 下次尝试 3数相乘.乘(t1, t2, t3)
+
     val 乘数1 = 乘数T1.fromInt(47)
     val 乘数2 = 乘数T2.fromInt(31)
     val 乘数3 = 乘数T3.fromInt(23)
