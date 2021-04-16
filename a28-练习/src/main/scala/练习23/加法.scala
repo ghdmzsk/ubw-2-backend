55ef8f0fa95762004aa07e0a0dd86a018e6baade
djx271 <2066473905@qq.com>
2021-04-17 05:02:12 +0800

    sdf

---
+++ a28-练习/src/main/scala/练习23/加法.scala
@@ -0,0 +1,34 @@
+package 练习23
+
+trait Summand {
+  def length: Int
+}
+object Summand {
+  def fromInt(n: Int): Summand = n match {
+    case n1 if n1 > 0 => new PositiveSummand(fromInt(n1 - 1))
+    case 0            => new ZeroSummand
+  }
+  def sum(summand: Summand, addend: Addend) = addend.plus(summand)
+}
+class PositiveSummand(tail: Summand) extends Summand {
+  override def length: Int = tail.length + 1
+}
+class ZeroSummand extends Summand {
+  override def length: Int = 0
+}
+
+trait Addend {
+  def plus(summand: Summand): Summand
+}
+object Addend {
+  def fromInt(n: Int): Addend = n match {
+    case n1 if n1 > 0 => new PositiveAddend(fromInt(n1 - 1))
+    case 0            => new ZeroAddend
+  }
+}
+class PositiveAddend(tail: Addend) extends Addend {
+  override def plus(summand: Summand): Summand = new PositiveSummand(tail.plus(summand))
+}
+class ZeroAddend extends Addend {
+  override def plus(summand: Summand): Summand = summand
+}
