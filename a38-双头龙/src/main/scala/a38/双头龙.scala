57d40f0b166c9ee93083da6cf2b1985e8d715d98
djx314 <djx314@sina.cn>
2021-06-03 09:38:44 +0800

    Commit.

--- a38-双头龙/src/main/scala/a38/双头龙.scala
+++ a38-双头龙/src/main/scala/a38/双头龙.scala
@@ -1,3 +1,15 @@
 package a38
 
-object bb
+trait Number1
+trait Number2
+
+case class Number3_1(tail: Number2) extends Number1
+case class Number3_2(tail: Number2) extends Number1
+
+case class Number4_Middle_1(tail: Number2)
+case class Number4_Middle_2(tail: Number2)
+case class Number4_Middle_3(tail: Number2)
+
+case object Number4_Bottom_1
+case object Number4_Bottom_2
+case object Number4_Bottom_3
