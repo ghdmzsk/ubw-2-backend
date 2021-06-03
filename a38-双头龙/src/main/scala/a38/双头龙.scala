6e732909158c5bf1e1efc05e40a16fcc6e62b1fa
djx314 <djx314@sina.cn>
2021-06-03 11:59:09 +0800

    Commit.

--- a38-双头龙/src/main/scala/a38/双头龙.scala
+++ a38-双头龙/src/main/scala/a38/双头龙.scala
@@ -42,12 +42,6 @@ case class Number4_Middle_3(tail: Number1) extends Number1 {
     Value(value = math.pow(3, length).toInt * 2 + tail.value.value, literal = lit)
   }
 }
-case class Number4_Middle_1_In(tail: Number1) extends Number1 {
-  override def value: Value = {
-    val lit = "0" + tail.value.literal
-    Value(value = tail.value.value, literal = lit)
-  }
-}
 
 case object Number4_Bottom_1 extends Number1 {
   override def value: Value = Value(value = 0, literal = "0")
@@ -61,48 +55,46 @@ case object Number4_Bottom_3 extends Number1 {
 
 object Number5 {
   def len(num: Number1): Int = num match {
-    case Number3_1(tail)           => len(tail) + 1
-    case Number3_2(tail)           => len(tail) + 1
-    case Number4_Middle_1(tail)    => len(tail) + 1
-    case Number4_Middle_2(tail)    => len(tail) + 1
-    case Number4_Middle_3(tail)    => len(tail) + 1
-    case Number4_Middle_1_In(tail) => len(tail) + 1
-    case Number4_Bottom_1          => 1
-    case Number4_Bottom_2          => 1
-    case Number4_Bottom_3          => 1
+    case Number3_1(tail)        => len(tail) + 1
+    case Number3_2(tail)        => len(tail) + 1
+    case Number4_Middle_1(tail) => len(tail) + 1
+    case Number4_Middle_2(tail) => len(tail) + 1
+    case Number4_Middle_3(tail) => len(tail) + 1
+    case Number4_Bottom_1       => 1
+    case Number4_Bottom_2       => 1
+    case Number4_Bottom_3       => 1
   }
 
   def method1(number1: Number1): Number1 = {
     number1 match {
       case Number3_1(tail) =>
         method1(tail) match {
-          case Number4_Middle_1_In(tail) => Number3_2(tail)
-          case i                         => Number3_1(i)
+          case Number3_1(tail) => Number3_2(tail)
+          case i               => Number3_1(i)
         }
       case Number3_2(tail) =>
         method1(tail) match {
-          case i @ Number4_Middle_1_In(tail) => Number3_1(i)
-          case i                             => Number3_2(i)
+          case Number3_1(tail) => Number3_1(Number4_Middle_1(tail))
+          case i               => Number3_2(i)
         }
       case Number4_Middle_1(tail) =>
         method1(tail) match {
-          case Number4_Middle_1_In(tail) => Number4_Middle_2(tail)
-          case i                         => Number4_Middle_1(i)
+          case Number3_1(tail) => Number4_Middle_2(tail)
+          case i               => Number4_Middle_1(i)
         }
       case Number4_Middle_2(tail) =>
         method1(tail) match {
-          case Number4_Middle_1_In(tail) => Number4_Middle_3(tail)
-          case i                         => Number4_Middle_2(i)
+          case Number3_1(tail) => Number4_Middle_3(tail)
+          case i               => Number4_Middle_2(i)
         }
       case Number4_Middle_3(tail) =>
         method1(tail) match {
-          case i @ Number4_Middle_1_In(tail) => Number4_Middle_1_In(i)
-          case i                             => Number4_Middle_3(i)
+          case Number3_1(tail) => Number3_1(Number4_Middle_1(tail))
+          case i               => Number4_Middle_3(i)
         }
-      case Number4_Middle_1_In(tail) => Number4_Middle_1(method1(tail))
-      case Number4_Bottom_1          => Number4_Bottom_2
-      case Number4_Bottom_2          => Number4_Bottom_3
-      case Number4_Bottom_3          => Number4_Middle_1_In(Number4_Bottom_1)
+      case Number4_Bottom_1 => Number4_Bottom_2
+      case Number4_Bottom_2 => Number4_Bottom_3
+      case Number4_Bottom_3 => Number3_1(Number4_Bottom_1)
     }
   }
 
