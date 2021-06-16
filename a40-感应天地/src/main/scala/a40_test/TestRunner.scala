f5032d8b3bd464ad7bafee72531a6455398d2616
djx314 <djx314@sina.cn>
2021-06-16 17:28:11 +0800

    Commit.

---
+++ a40-感应天地/src/main/scala/a40_test/TestRunner.scala
@@ -0,0 +1,10 @@
+package a40_test
+
+object Runner {
+
+  def main(arr: Array[String]): Unit = {
+    println(TopNumber2(MiddleNumber2(MiddleNumber2(MiddleNumber2(BottomNumber2)))).method1.length) // 242
+    println(TopNumber1(MiddleNumber2(MiddleNumber1(MiddleNumber0(BottomNumber2)))).method1.length) // 146
+  }
+
+}
