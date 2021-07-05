a5be49ffd4ac9d5846656219e8f5a96f4ae2ce5b
水山清风 <djx314@sina.cn>
2021-07-05 12:13:37 +0000

    update a42-stream/src/main/scala/step1/Stream.scala.

--- a42-stream/src/main/scala/step1/Stream.scala
+++ a42-stream/src/main/scala/step1/Stream.scala
@@ -17,28 +17,36 @@ trait Number3 extends Number1 {
   override def send(item: Item): Number2
   override def end: Number2
 }
-case class Number3Outter(tail: Number3, head: Item, next: Number1) extends Number3 {
+case class Number3Positive(tail: Number3, head: Item) extends Number3 {
   override def send(item: Item): Number2
   override def end: Number2
 }
-case class Number3Inner(tail: Number3, head: Item) extends Number3 {
-  override def send(item: Item): Number2
-  override def end: Number2
-}
-trait Nubmer3Zero extends Number1 {
+case object Nubmer3Zero extends Number3 {
   override def send(item: Item): Number2
   override def end: Number2 = Number2Zero
 }
 
-trait Number2
-case object EndNumber2 extends Number2
-case class Number2Positive(tail: Number2, head: Item, next: Number2) extends Number2 {
-  def append = next.append(head)
-  def end    = tail.append(head)
+trait Number4 extends Number1 {
+  override def send(item: Item): Number2
+  override def end: Number2
 }
-case class Number2Zero(next: Number2) extends Number2 {
-  def append = next.end
-  def end    = next.end
+case class Number4Outter(tail: Number3, head: Item, next: Number1) extends Number4 {
+  override def send(item: Item): Number2
+  override def end: Number2
 }
+case class Number4Positive(tail: Number2, head: Item, next: Number2) extends Number4 {
+  override def send(item: Item): Number2
+  override def end: Number2
+}
+case class Number4Zero(next: Number2) extends Number4 {
+  override def send(item: Item): Number2
+  override def end: Number2
+}
+
+trait Monad1
+
+trait Monad3 extends Monad1
+case class Monad3Positive extends Monad3
+case class Monad3Zero extends Monad3
 
 println(new Number2 { })
