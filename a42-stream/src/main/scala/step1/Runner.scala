0e5ddd4f63214632e46db03250b2f29614f1431d
djx314 <djx314@sina.cn>
2021-07-06 01:27:33 +0800

    sdfwse

--- a42-stream/src/main/scala/step1/Runner.scala
+++ a42-stream/src/main/scala/step1/Runner.scala
@@ -2,10 +2,29 @@ package step1
 
 object Runner extends App {
 
-  val number1 = FirstNumberPositive(FirstNumberPositive(FirstNumberPositive(FirstNumberZero)))
-  val number2 = Number4Positive(Number4Positive(Number4Positive(Number4Positive(Number4Zero))))
+  val item01 = Item("Item01")
+  val item02 = Item("Item02")
+  val item03 = Item("Item03")
+  val item04 = Item("Item04")
+  val item05 = Item("Item05")
+  val item06 = Item("Item06")
+  val item07 = Item("Item07")
+  val item08 = Item("Item08")
+  val item09 = Item("Item09")
+  val item10 = Item("Item10")
+  val item11 = Item("Item11")
+  val item12 = Item("Item12")
 
-  val count = StreamPositive(StreamPositive(StreamPositive(StreamPositive(StreamZero, number1), number2), number2), number2)
+  val number1 = FirstNumberPositive(FirstNumberPositive(FirstNumberPositive(FirstNumberZero, item01), item02), item03)
+  val number2 = PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item04), item05), item06), item07)
+  val number3 = PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item08), item09)
+  val number4 = PlusNumberPositive(PlusNumberZero, item10)
+  val number5 = PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item11), item12)
+
+  val count = ReverseStreamPositive(
+    ReverseStreamPositive(ReverseStreamPositive(ReverseStreamPositive(ReverseStreamPositive(ReverseStreamZero, number1), number2), number3), number4),
+    number5
+  )
 
   println(count.run)
 
--- a42-stream/src/main/scala/step1/Stream.scala
+++ a42-stream/src/main/scala/step1/Stream.scala
@@ -2,58 +2,59 @@ package step1
 
 case class Item(name: String)
 
-trait Number1 {
-  def next(stream: Stream, reverseStream: ReverseStream): Number2
+trait Number {
+  def next(stream: Stream, reverseStream: ReverseStream, item: Item): Number2
+}
+
+trait ReverseNumber {
   def reverse(stream: Stream, reverseStream: ReverseStream): Number2
 }
 
-trait Number2
-case class Number2Positive(tail: Number2) extends Number2
-case object Number2Zero                   extends Number2
+trait Number2 {}
+case class Number2Positive(tail: Number2, head: Item) extends Number2 {
+  override def toString: String = s"(${tail}, ${head.name})"
+}
+case object Number2Zero extends Number2 {
+  override def toString: String = "Zero"
+}
 
-trait FirstNumber extends Number1 {
-  override def next(stream: Stream, reverseStream: ReverseStream): Number2
+trait FirstNumber extends ReverseNumber {
   override def reverse(stream: Stream, reverseStream: ReverseStream): Number2
 }
-case class FirstNumberPositive(tail: FirstNumber) extends FirstNumber {
-  override def next(stream: Stream, reverseStream: ReverseStream): Number2    = stream.next(ReverseStreamPositive(reverseStream, tail))
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2 = stream.next(ReverseStreamPositive(reverseStream, tail))
+case class FirstNumberPositive(tail: FirstNumber, head: Item) extends FirstNumber {
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2 = stream.next(ReverseStreamPositive(reverseStream, tail), head)
 }
 case object FirstNumberZero extends FirstNumber {
-  override def next(stream: Stream, reverseStream: ReverseStream): Number2    = stream.next(reverseStream)
   override def reverse(stream: Stream, reverseStream: ReverseStream): Number2 = reverseStream.reverse(stream)
 }
 
-trait Number4 extends Number1 {
-  override def next(stream: Stream, reverseStream: ReverseStream): Number2
+trait PlusNumber extends ReverseNumber {
   override def reverse(stream: Stream, reverseStream: ReverseStream): Number2
 }
-case class Number4Positive(tail: Number4) extends Number4 {
-  override def next(stream: Stream, reverseStream: ReverseStream): Number2    = stream.next(ReverseStreamPositive(reverseStream, Number4Positive(tail)))
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2 = stream.next(ReverseStreamPositive(reverseStream, tail))
+case class PlusNumberPositive(tail: PlusNumber, head: Item) extends PlusNumber {
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2 = stream.next(ReverseStreamPositive(reverseStream, tail), head)
 }
-case object Number4Zero extends Number4 {
-  override def next(stream: Stream, reverseStream: ReverseStream): Number2    = stream.next(reverseStream)
+case object PlusNumberZero extends PlusNumber {
   override def reverse(stream: Stream, reverseStream: ReverseStream): Number2 = reverseStream.reverse(stream)
 }
 
 trait Stream {
-  def next(reverseStream: ReverseStream): Number2
+  def next(reverseStream: ReverseStream, item: Item): Number2
 }
-case class StreamPositive(tail: Stream, number1: Number1) extends Stream {
-  def next(reverseStream: ReverseStream): Number2 = number1.next(tail, reverseStream)
-  def run: Number2                                = number1.next(tail, ReverseStreamZero)
+case class StreamPositive(tail: Stream, number1: Number) extends Stream {
+  override def next(reverseStream: ReverseStream, item: Item): Number2 = number1.next(tail, reverseStream, item)
 }
 case object StreamZero extends Stream {
-  def next(reverseStream: ReverseStream): Number2 = Number2Positive(reverseStream.reverse(StreamZero))
+  override def next(reverseStream: ReverseStream, item: Item): Number2 = Number2Positive(reverseStream.reverse(StreamZero), item)
 }
 
 trait ReverseStream {
   def reverse(stream: Stream): Number2
 }
-case class ReverseStreamPositive(tail: ReverseStream, number1: Number1) extends ReverseStream {
-  def reverse(stream: Stream): Number2 = number1.reverse(stream, tail)
+case class ReverseStreamPositive(tail: ReverseStream, number1: ReverseNumber) extends ReverseStream {
+  override def reverse(stream: Stream): Number2 = number1.reverse(stream, tail)
+  def run: Number2                              = reverse(StreamZero)
 }
 case object ReverseStreamZero extends ReverseStream {
-  def reverse(stream: Stream): Number2 = Number2Zero
+  override def reverse(stream: Stream): Number2 = Number2Zero
 }
