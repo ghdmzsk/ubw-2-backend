ea884643290c3ed773e375d353156bdd4653b432
水山清风 <djx314@sina.cn>
2021-07-05 13:25:50 +0000

    update a42-stream/src/main/scala/step1/Stream.scala.

--- a42-stream/src/main/scala/step1/Stream.scala
+++ a42-stream/src/main/scala/step1/Stream.scala
@@ -1,41 +1,41 @@
 case class Item(name: String)
 
 trait Number1 {
-  def send(item: Item): Number2
-  def end: Number2
+  def next(monadA: Monad1, monadB: Monad1): Number2
+  def reverse(monadA: Monad1, monadB: Monad1): Number2
 }
 case object Number1Zero {
-  def send(item: Item): Number2
-  def end: Number2
+  def next(monadA: Monad1, monadB: Monad1): Number2 = Number2Zero
+  def reverse(monadA: Monad1, monadB: Monad1): Number2 = Number2Zero
 }
 
 trait Number2
 case class Number2Positive(tail: Number2) extends Number2
-case object Number2Zero extends Nubmer2
+case object Number2Zero extends Number2
 
 trait Number4 extends Number1 {
-  override def next(monad3: Monad3): Number2
-  override def reverse(monad4: Monad4): Number2
+  override def next(monadA: Monad1, monadB: Monad1): Number2 = Number2Zero
+  override def reverse(monadA: Monad1, monadB: Monad1): Number2 = Number2Zero
 }
 case class Number4Positive(tail: Number2) extends Number4 {
-  override def next(monad3: Monad3): Number2 = Number2Positive(tail.send)
-  override def reverse(monad4: Monad4): Number2
+  override def next(monadA: Monad1, monadB: Monad1): Number2 = Number2Zero
+  override def reverse(monadA: Monad1, monadB: Monad1): Number2 = Number2Zero
 }
 case class Number4Zero(next: Number2) extends Number4 {
-  override def next(monad3: Monad3): Number2
-  override def reverse(monad4: Monad4): Number2
+  override def next(monadA: Monad1, monadB: Monad1): Number2 = Number2Zero
+  override def reverse(monadA: Monad1, monadB: Monad1): Number2 = Number2Zero
 }
 
 trait Monad1 {
-  def next(monad4: Monad4): Number2
+  def next(monad4: Monad1): Number2
   def reverse(monad1: Monad1): Number2
 }
 case class Monad1Positive(tail: Monad1, number1: Number1) extends Monad1 {
   def next(monad1: Monad1): Number2 = number1.next(tail, monad1)
-  def reverse(monad1: Monad1): Number2 = number.reverse(monad1, tail)
+  def reverse(monad1: Monad1): Number2 = number1.reverse(monad1, tail)
 }
 case object Monad1Zero extends Monad1 {
-  def next(monad1: Monad1): Number2 = Number2Positive(monad4.reverse(Monad3Zero))
+  def next(monad1: Monad1): Number2 = Number2Positive(monad1.reverse(Monad1Zero))
   def reverse(monad1: Monad1): Number2 = monad1.next(Monad1Zero)
 }
 
