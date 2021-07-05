b0ed3d5a3fb42c1edf53e93c738f2e6b1c9c7227
djx314 <djx314@sina.cn>
2021-07-06 01:49:31 +0800

    Commit.

--- a42-stream/src/main/scala/step1/Stream.scala
+++ a42-stream/src/main/scala/step1/Stream.scala
@@ -3,71 +3,71 @@ package step1
 case class Item(name: String)
 
 trait Number {
-  def next(stream: Stream, reverseStream: ReverseStream, item: Item): Number2
+  def next(stream: Stream, reverseStream: ReverseStream, item: Item): Result
 }
 
 trait ReverseNumber {
-  def reverse(stream: Stream, reverseStream: ReverseStream): Number2
+  def reverse(stream: Stream, reverseStream: ReverseStream): Result
 }
 
-trait Number2
-case class Number2Positive(tail: Number2, head: Item) extends Number2 {
+trait Result
+case class ResultPositive(tail: Result, head: Item) extends Result {
   override def toString: String = s"(${tail}, ${head.name})"
 }
-case object Number2Zero extends Number2 {
+case object ResultZero extends Result {
   override def toString: String = "Zero"
 }
 
 trait FirstNumber extends ReverseNumber {
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Result
 }
 case class FirstNumberPositive(tail: FirstNumber, head: Item) extends FirstNumber {
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2 = stream.next(ReverseStreamPositive(reverseStream, tail), head)
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Result = stream.next(ReverseStreamPositive(reverseStream, tail), head)
 }
 case object FirstNumberZero extends FirstNumber {
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2 = reverseStream.reverse(stream)
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Result = reverseStream.reverse(stream)
 }
 
 trait PlusNumber extends ReverseNumber {
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Result
 }
 case class PlusNumberPositive(tail: PlusNumber, head: Item) extends PlusNumber {
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2 = stream.next(ReverseStreamPositive(reverseStream, tail), head)
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Result = stream.next(ReverseStreamPositive(reverseStream, tail), head)
 }
 case object PlusNumberZero extends PlusNumber {
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2 = reverseStream.reverse(stream)
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Result = reverseStream.reverse(stream)
 }
 
 trait MinusNumber extends Number with ReverseNumber {
-  override def next(stream: Stream, reverseStream: ReverseStream, item: Item): Number2
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2
+  override def next(stream: Stream, reverseStream: ReverseStream, item: Item): Result
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Result
 }
 case class MinusNumberPositive(tail: MinusNumber) extends Number with MinusNumber {
-  override def next(stream: Stream, reverseStream: ReverseStream, item: Item): Number2 = reverseStream.reverse(StreamPositive(stream, tail))
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2          = reverseStream.reverse(StreamPositive(stream, MinusNumberPositive(tail)))
+  override def next(stream: Stream, reverseStream: ReverseStream, item: Item): Result = reverseStream.reverse(StreamPositive(stream, tail))
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Result          = reverseStream.reverse(StreamPositive(stream, MinusNumberPositive(tail)))
 }
 case object MinusNumberZero extends Number with MinusNumber {
-  override def next(stream: Stream, reverseStream: ReverseStream, item: Item): Number2 = stream.next(reverseStream, item)
-  override def reverse(stream: Stream, reverseStream: ReverseStream): Number2          = reverseStream.reverse(stream)
+  override def next(stream: Stream, reverseStream: ReverseStream, item: Item): Result = stream.next(reverseStream, item)
+  override def reverse(stream: Stream, reverseStream: ReverseStream): Result          = reverseStream.reverse(stream)
 }
 
 trait Stream {
-  def next(reverseStream: ReverseStream, item: Item): Number2
+  def next(reverseStream: ReverseStream, item: Item): Result
 }
-case class StreamPositive(tail: Stream, number1: Number) extends Stream {
-  override def next(reverseStream: ReverseStream, item: Item): Number2 = number1.next(tail, reverseStream, item)
+case class StreamPositive(tail: Stream, number: Number) extends Stream {
+  override def next(reverseStream: ReverseStream, item: Item): Result = number.next(tail, reverseStream, item)
 }
 case object StreamZero extends Stream {
-  override def next(reverseStream: ReverseStream, item: Item): Number2 = Number2Positive(reverseStream.reverse(StreamZero), item)
+  override def next(reverseStream: ReverseStream, item: Item): Result = ResultPositive(reverseStream.reverse(StreamZero), item)
 }
 
 trait ReverseStream {
-  def reverse(stream: Stream): Number2
+  def reverse(stream: Stream): Result
 }
-case class ReverseStreamPositive(tail: ReverseStream, number1: ReverseNumber) extends ReverseStream {
-  override def reverse(stream: Stream): Number2 = number1.reverse(stream, tail)
-  def run: Number2                              = reverse(StreamZero)
+case class ReverseStreamPositive(tail: ReverseStream, reverseNumber: ReverseNumber) extends ReverseStream {
+  override def reverse(stream: Stream): Result = reverseNumber.reverse(stream, tail)
+  def run: Result                              = reverse(StreamZero)
 }
 case object ReverseStreamZero extends ReverseStream {
-  override def reverse(stream: Stream): Number2 = Number2Zero
+  override def reverse(stream: Stream): Result = ResultZero
 }
