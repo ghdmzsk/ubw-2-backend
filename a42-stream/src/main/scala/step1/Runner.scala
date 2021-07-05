c286f06b65785c8e4eec30af9c29b9a4acfbeea2
djx314 <djx314@sina.cn>
2021-07-06 02:02:48 +0800

    Commit.

--- a42-stream/src/main/scala/step1/Runner.scala
+++ a42-stream/src/main/scala/step1/Runner.scala
@@ -15,16 +15,22 @@ object Runner extends App {
   val item11 = Item("Item11")
   val item12 = Item("Item12")
 
-  val number1 = FirstNumberPositive(FirstNumberPositive(FirstNumberPositive(FirstNumberZero, item01), item02), item03)
-  val number2 = PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item04), item05), item06), item07)
-  val number3 = PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item08), item09)
-  val number4 = PlusNumberPositive(PlusNumberZero, item10)
-  val number5 = PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item11), item12)
+  def minusFromNum(n: Int): MinusNumber = n match {
+    case i if i > 0 => MinusNumberPositive(minusFromNum(n - 1))
+    case 0          => MinusNumberZero
+  }
+
+  val number1 = FirstNumberPositive(FirstNumberPositive(FirstNumberPositive(FirstNumberZero, item01), item02), item03)                         // 3
+  val number2 = PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item04), item05), item06), item07) // 4
+  val number3 = PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item08), item09)                                                         // 2
+  val number4 = PlusNumberPositive(PlusNumberZero, item10)                                                                                     // 1
+  val number5 = PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item11), item12)                                                         // 2
 
   val count1 = ReverseStreamPositive(
     ReverseStreamPositive(ReverseStreamPositive(ReverseStreamPositive(ReverseStreamPositive(ReverseStreamZero, number1), number2), number3), number4),
     number5
   )
+  // 3 + 4 + 2 + 1 + 2 = 12
 
   val number6 = MinusNumberPositive(MinusNumberPositive(MinusNumberZero))
 
@@ -35,11 +41,53 @@ object Runner extends App {
     ),
     number4
   )
+  // 3 + 4 + 2 + 2 - 2 + 1 = 10
+
+  def number7 = minusFromNum(200)
+
+  val count3 = ReverseStreamPositive(
+    ReverseStreamPositive(
+      ReverseStreamPositive(
+        ReverseStreamPositive(
+          ReverseStreamPositive(
+            ReverseStreamPositive(
+              ReverseStreamPositive(
+                ReverseStreamPositive(ReverseStreamPositive(ReverseStreamPositive(ReverseStreamPositive(ReverseStreamZero, number1), number2), number3), number3),
+                number3
+              ),
+              number3
+            ),
+            number3
+          ),
+          number3
+        ),
+        number7
+      ),
+      number4
+    ),
+    number5
+  )
+  // 3 + 4 + 2 + 2 + 2 + 2 + 2 + 2 - 200 + 1 + 2 = 3
+
+  val count4 = ReverseStreamPositive(
+    ReverseStreamPositive(
+      ReverseStreamPositive(ReverseStreamPositive(ReverseStreamPositive(ReverseStreamPositive(ReverseStreamZero, number1), number2), number3), number5),
+      number4
+    ),
+    number7
+  )
+  // 3 + 4 + 2 + 2 + 1 - 200 = 0
 
   println(count1.run)
   // ((((((((((((Zero, Item01), Item02), Item03), Item04), Item05), Item06), Item07), Item08), Item09), Item10), Item11), Item12)
 
   println(count2.run)
   // ((((((((((Zero, Item01), Item02), Item03), Item04), Item05), Item06), Item07), Item08), Item09), Item10)
+
+  println(count3.run)
+  // (((Zero, Item10), Item11), Item12)
+
+  println(count4.run)
+  // Zero
 
 }
