b41349a01bb9fd0f254045066ad40b2733e22278
djx314 <djx314@sina.cn>
2021-07-07 00:22:03 +0800

    sdf

--- a42-stream/src/main/scala/step2/Stream.scala
+++ a42-stream/src/main/scala/step2/Stream.scala
@@ -16,8 +16,24 @@ case object ResultZero                              extends Result
 
 trait PlusNumber extends ReverseNumber
 case class PlusNumberPositive(tail: PlusNumber, head: Item) extends PlusNumber {
-  override def reverse(number: Number): Result = ResultZero
+  override def reverse(number: Number): Result = number.next(tail, head)
 }
 case object PlusNumberZero extends PlusNumber {
+  override def reverse(number: Number): Result = ResultZero
+}
+
+trait LinkNumber extends Number
+case class LinkNumberPositive(tail: LinkNumber, head: Number) extends LinkNumber {
+  override def next(reverseNumber: ReverseNumber, item: Item): Result = ResultZero
+}
+case object LinkNumberZero extends LinkNumber {
+  override def next(reverseNumber: ReverseNumber, item: Item): Result = ResultZero
+}
+
+trait ReverseLinkNumber extends ReverseNumber
+case class ReverseLinkNumberPositive(tail: ReverseLinkNumber, head: ReverseNumber) extends ReverseLinkNumber {
+  override def reverse(number: Number): Result = ResultZero
+}
+case object ReverseLinkNumberZero extends ReverseLinkNumber {
   override def reverse(number: Number): Result = ResultZero
 }
