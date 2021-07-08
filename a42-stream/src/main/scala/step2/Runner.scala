1232a617c02f9baf68353c52d690e692f2d590a2
djx314 <djx314@sina.cn>
2021-07-09 01:33:04 +0800

    hjiohjihujibhetujierujitberuy

--- a42-stream/src/main/scala/step2/Runner.scala
+++ a42-stream/src/main/scala/step2/Runner.scala
@@ -21,14 +21,18 @@ object Runner extends App {
   // + 5
   val number3 = PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item09), item10), item11), item12)
   // + 4
+  val number4 = MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberZero)))
+  // - 3
+  val number5 = MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberZero)))))
+  // - 5
+  val number6 = MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberZero))))
+  // - 4
 
   val count1 = 被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Zero, number1), number2), number3)
   // 3 + 5 + 4 = 12
+
   println(count1.向左计算)
   // ((((((((((((Zero, Item(Item01)), Item(Item02)), Item(Item03)), Item(Item04)), Item(Item05)), Item(Item06)), Item(Item07)), Item(Item08)), Item(Item09)), Item(Item10)), Item(Item11)), Item(Item12))
-
-  val number4 = MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberZero)))
-// - 3
 
   val count2 = 被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Zero, number1), number2), number3), number4)
   // 3 + 5 + 4 - 3 = 9
@@ -42,16 +46,11 @@ object Runner extends App {
   println(count3.向左计算)
   // (((((((((Zero, Item(Item01)), Item(Item02)), Item(Item03)), Item(Item04)), Item(Item05)), Item(Item09)), Item(Item10)), Item(Item11)), Item(Item12))
 
-  val number5 = MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberZero)))))
-  // - 5
-
   val count4 = 被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Zero, number1), 被动消耗Positive(被动消耗Positive(被动消耗Zero, number2), number4)), number3), number5)
   // 3 + (5 - 3) + 4 - 5 = 4
 
   println(count4.向左计算)
   // ((((Zero, Item(Item01)), Item(Item02)), Item(Item03)), Item(Item04))
-
-  val number6 = MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberZero))))
 
   val count5 = 被动消耗Positive(
     被动消耗Positive(被动消耗Positive(被动消耗Zero, number1), 被动消耗Positive(被动消耗Positive(被动消耗Zero, number2), number4)),
