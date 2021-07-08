6463791edd07062f63b9b53552e7aa7be8d67d9e
djx314 <djx314@sina.cn>
2021-07-09 01:23:04 +0800

    Commit.

--- a42-stream/src/main/scala/step2/Runner.scala
+++ a42-stream/src/main/scala/step2/Runner.scala
@@ -16,10 +16,50 @@ object Runner extends App {
   val item12 = Item("Item12")
 
   val number1 = PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item01), item02), item03)
+  // + 3
   val number2 = PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item04), item05), item06), item07), item08)
+  // + 5
   val number3 = PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberPositive(PlusNumberZero, item09), item10), item11), item12)
-  // println(number1.计算)
-  // println(number2.计算)
-  println(被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Zero, number1), number2), number1).向左计算)
+  // + 4
+
+  val count1 = 被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Zero, number1), number2), number3)
+  // 3 + 5 + 4 = 12
+  println(count1.向左计算)
+  // ((((((((((((Zero, Item(Item01)), Item(Item02)), Item(Item03)), Item(Item04)), Item(Item05)), Item(Item06)), Item(Item07)), Item(Item08)), Item(Item09)), Item(Item10)), Item(Item11)), Item(Item12))
+
+  val number4 = MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberZero)))
+// - 3
+
+  val count2 = 被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Zero, number1), number2), number3), number4)
+  // 3 + 5 + 4 - 3 = 9
+
+  println(count2.向左计算)
+  // (((((((((Zero, Item(Item01)), Item(Item02)), Item(Item03)), Item(Item04)), Item(Item05)), Item(Item06)), Item(Item07)), Item(Item08)), Item(Item09))
+
+  val count3 = 被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Zero, number1), 被动消耗Positive(被动消耗Positive(被动消耗Zero, number2), number4)), number3)
+  // 3 + (5 - 3) + 4 = 9
+
+  println(count3.向左计算)
+  // (((((((((Zero, Item(Item01)), Item(Item02)), Item(Item03)), Item(Item04)), Item(Item05)), Item(Item09)), Item(Item10)), Item(Item11)), Item(Item12))
+
+  val number5 = MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberZero)))))
+  // - 5
+
+  val count4 = 被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Positive(被动消耗Zero, number1), 被动消耗Positive(被动消耗Positive(被动消耗Zero, number2), number4)), number3), number5)
+  // 3 + (5 - 3) + 4 - 5 = 4
+
+  println(count4.向左计算)
+  // ((((Zero, Item(Item01)), Item(Item02)), Item(Item03)), Item(Item04))
+
+  val number6 = MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberPositive(MinusNumberZero))))
+
+  val count5 = 被动消耗Positive(
+    被动消耗Positive(被动消耗Positive(被动消耗Zero, number1), 被动消耗Positive(被动消耗Positive(被动消耗Zero, number2), number4)),
+    被动消耗Positive(被动消耗Positive(被动消耗Zero, number3), number6)
+  )
+  // 3 + (5 - 3) + (4 - 4) = 5
+
+  println(count5.向左计算)
+  // (((((Zero, Item(Item01)), Item(Item02)), Item(Item03)), Item(Item04)), Item(Item05))
 
 }
--- a42-stream/src/main/scala/step2/Stream.scala
+++ a42-stream/src/main/scala/step2/Stream.scala
@@ -4,82 +4,89 @@ case class Item(name: String)
 
 trait 数 {
   def 向左走(number: 数): Result
-  def 向右走(number: 数): Result
+  def 向右走(number: 数, item: Item): Result
   def 向左计算: Result
-  def 向右计算: Result
+  def 向右计算(item: Item): Result
 }
 
 trait Result
-case class ResultPositive(value: Result) extends Result
-case object ResultZero                   extends Result
+case class ResultPositive(value: Result, item: Item) extends Result {
+  override def toString: String = s"($value, $item)"
+}
+case object ResultZero extends Result {
+  override def toString: String = "Zero"
+}
 
 trait PlusNumber extends 数 {
   override def 向左走(大壳: 数): Result
-  override def 向右走(大壳: 数): Result
+  override def 向右走(大壳: 数, item: Item): Result
   override def 向左计算: Result
-  override def 向右计算: Result
+  override def 向右计算(item: Item): Result
 }
 case class PlusNumberPositive(tail: PlusNumber, head: Item) extends PlusNumber {
-  override def 向左走(大壳: 数): Result = 大壳.向右走(tail)
-  override def 向右走(大壳: 数): Result = 大壳.向右走(PlusNumberPositive(tail, head))
-  override def 向左计算: Result       = ResultPositive(tail.向左计算)
-  override def 向右计算: Result       = ResultPositive(tail.向右计算)
+  override def 向左走(大壳: 数): Result             = 大壳.向右走(tail, head)
+  override def 向右走(大壳: 数, item: Item): Result = 大壳.向右走(PlusNumberPositive(tail, head), item)
+  override def 向左计算: Result                   = ResultPositive(tail.向左计算, head)
+  override def 向右计算(item: Item): Result       = ResultPositive(tail.向右计算(item), head)
 }
 case object PlusNumberZero extends PlusNumber {
-  override def 向左走(大壳: 数): Result = 大壳.向左计算
-  override def 向右走(大壳: 数): Result = 大壳.向右计算
-  override def 向左计算: Result       = ResultZero
-  override def 向右计算: Result       = ResultZero
+  override def 向左走(大壳: 数): Result             = 大壳.向左计算
+  override def 向右走(大壳: 数, item: Item): Result = 大壳.向右计算(item)
+  override def 向左计算: Result                   = ResultZero
+  override def 向右计算(item: Item): Result       = ResultPositive(ResultZero, item)
 }
 
-/*trait MinusNumber extends 数 {
-  override def 正向(number: 数): Result
-  override def 逆向(number: 数): Result
-  override def 计算: Result
+trait MinusNumber extends 数 {
+  override def 向左走(大壳: 数): Result
+  override def 向右走(大壳: 数, item: Item): Result
+  override def 向左计算: Result
+  override def 向右计算(item: Item): Result
 }
 case class MinusNumberPositive(tail: MinusNumber) extends MinusNumber {
-  override def 正向(number: 数): Result = number.交缠(tail)
-  override def 逆向(number: 数): Result = number.相冲(tail)
-  override def 计算: Result            = ResultZero
+  override def 向左走(大壳: 数): Result             = 大壳.向左走(MinusNumberPositive(tail))
+  override def 向右走(大壳: 数, item: Item): Result = 大壳.向左走(tail)
+  override def 向左计算: Result                   = tail.向左计算
+  override def 向右计算(item: Item): Result       = tail.向右计算(item)
 }
 case object MinusNumberZero extends MinusNumber {
-  override def 正向(number: 数): Result = number.独自
-  override def 逆向(number: 数): Result = number.独自
-  override def 计算: Result            = ResultZero
-}*/
+  override def 向左走(大壳: 数): Result             = 大壳.向左计算
+  override def 向右走(大壳: 数, item: Item): Result = 大壳.向右计算(item)
+  override def 向左计算: Result                   = ResultZero
+  override def 向右计算(item: Item): Result       = ResultZero
+}
 
 trait 主动消耗 extends 数
 case class 主动消耗Positive(主动消耗Tail: 数, 计算数: 数) extends 主动消耗 {
-  override def 向左走(被动消耗: 数): Result = 计算数.向左走(都套大壳(被动消耗, 主动消耗Tail))
-  override def 向右走(被动消耗: 数): Result = 计算数.向右走(都套大壳(被动消耗, 主动消耗Tail))
-  override def 向左计算: Result         = 向左走(被动消耗Zero)
-  override def 向右计算: Result         = 向右走(被动消耗Zero)
+  override def 向左走(被动消耗: 数): Result             = 计算数.向左走(都套大壳(被动消耗, 主动消耗Tail))
+  override def 向右走(被动消耗: 数, item: Item): Result = 计算数.向右走(都套大壳(被动消耗, 主动消耗Tail), item)
+  override def 向左计算: Result                     = 向左走(被动消耗Zero)
+  override def 向右计算(item: Item): Result         = 向右走(被动消耗Zero, item)
 }
 case object 主动消耗Zero extends 主动消耗 {
-  override def 向左走(被动消耗: 数): Result = 被动消耗.向左计算
-  override def 向右走(被动消耗: 数): Result = ResultPositive(被动消耗.向左计算)
-  override def 向左计算: Result         = ResultZero
-  override def 向右计算: Result         = ResultZero
+  override def 向左走(被动消耗: 数): Result             = 被动消耗.向左计算
+  override def 向右走(被动消耗: 数, item: Item): Result = ResultPositive(被动消耗.向左计算, item)
+  override def 向左计算: Result                     = ResultZero
+  override def 向右计算(item: Item): Result         = ResultPositive(ResultZero, item)
 }
 
 trait 被动消耗 extends 数
 case class 被动消耗Positive(被动消耗Tail: 数, 计算数: 数) extends 被动消耗 {
-  override def 向左走(主动消耗: 数): Result = 计算数.向左走(都套大壳(被动消耗Tail, 主动消耗))
-  override def 向右走(主动消耗: 数): Result = 计算数.向右走(都套大壳(被动消耗Tail, 主动消耗))
-  override def 向左计算: Result         = 向左走(主动消耗Zero)
-  override def 向右计算: Result         = 向右走(主动消耗Zero)
+  override def 向左走(主动消耗: 数): Result             = 计算数.向左走(都套大壳(被动消耗Tail, 主动消耗))
+  override def 向右走(主动消耗: 数, item: Item): Result = 计算数.向右走(都套大壳(被动消耗Tail, 主动消耗), item)
+  override def 向左计算: Result                     = 向左走(主动消耗Zero)
+  override def 向右计算(item: Item): Result         = 向右走(主动消耗Zero, item)
 }
 case object 被动消耗Zero extends 被动消耗 {
-  override def 向左走(主动消耗: 数): Result = 主动消耗.向右计算
-  override def 向右走(主动消耗: 数): Result = 主动消耗.向右计算
-  override def 向左计算: Result         = ResultZero
-  override def 向右计算: Result         = ResultZero
+  override def 向左走(主动消耗: 数): Result             = 主动消耗.向左计算
+  override def 向右走(主动消耗: 数, item: Item): Result = 主动消耗.向右计算(item)
+  override def 向左计算: Result                     = ResultZero
+  override def 向右计算(item: Item): Result         = ResultPositive(ResultZero, item)
 }
 
 trait 大壳 extends 数
 case class 都套大壳(被动消耗: 数, 主动消耗: 数) extends 大壳 {
-  override def 向左走(number: 数): Result = 被动消耗.向左走(主动消耗Positive(主动消耗, number))
-  override def 向右走(number: 数): Result = 主动消耗.向右走(被动消耗Positive(被动消耗, number))
-  override def 向左计算: Result           = 被动消耗.向左走(主动消耗)
-  override def 向右计算: Result           = 主动消耗.向右走(被动消耗)
+  override def 向左走(number: 数): Result             = 被动消耗.向左走(主动消耗Positive(主动消耗, number))
+  override def 向右走(number: 数, item: Item): Result = 主动消耗.向右走(被动消耗Positive(被动消耗, number), item)
+  override def 向左计算: Result                       = 被动消耗.向左走(主动消耗)
+  override def 向右计算(item: Item): Result           = 主动消耗.向右走(被动消耗, item)
 }
