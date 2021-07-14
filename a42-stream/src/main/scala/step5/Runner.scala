5065c9b25ad63cd845163347e6cc9149b00966c7
djx314 <djx314@sina.cn>
2021-07-14 13:53:15 +0800

    加法。

--- a42-stream/src/main/scala/step5/Runner.scala
+++ a42-stream/src/main/scala/step5/Runner.scala
@@ -18,10 +18,17 @@ object Runner extends App {
 
   val number1 = 加法正(加法正(加法正(加法正(加法零, item01), item02), item03), item04)
   val number2 = 加法正(加法正(加法正(加法零, item05), item06), item07)
+  val number3 = 加法正(加法正(加法正(加法正(加法零, item08), item09), item10), item11)
+  val number4 = 加法正(加法零, item12)
 
-  val l = 黑左正右正(number2, number1)
+  val l1 = 黑左正右正(number2, number1)
 
-  println(Number.count(l))
+  println(Number.count(l1))
   // (((((((Zero, Item01), Item02), Item03), Item04), Item05), Item06), Item07)
+
+  val l2 = 黑左树右正(黑左树右正(黑左正右正(number4, number3), number2), number1)
+
+  println(Number.count(l2))
+  // ((((((((((((Zero, Item01), Item02), Item03), Item04), Item05), Item06), Item07), Item08), Item09), Item10), Item11), Item12)
 
 }
