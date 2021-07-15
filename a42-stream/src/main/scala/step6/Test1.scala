8e39af2012962f2270f8bf2c9daebe73ddf3b03c
水山清风 <djx314@sina.cn>
2021-07-15 07:52:34 +0000

    update a42-stream/src/main/scala/step6/Test1.scala.

--- a42-stream/src/main/scala/step6/Test1.scala
+++ a42-stream/src/main/scala/step6/Test1.scala
@@ -23,7 +23,7 @@ trait 黑树 {
 }
 
 case class BlackTree(left: Number1, f: 法, right: Number1) extends 黑树 {
-  override def 计算: Result = f.计算(双白树(left = left, right = right))
+  override def 计算: Result = f.右侧请求(双白树(left = left, right = right))
 }
 
 trait 白树 {
@@ -32,16 +32,37 @@ trait 白树 {
 case class 双白树(left: Number1, right: Number1) extends 白树 {
   override def 向右获取(f: 法): Result = right.被索取(左法半树(f = f, left = left))
 }
+case class 左白树(left: Number1) extends 白树 {
+  override def 向右获取(f: 法): Result = left.被索取(左法半树(f = f, left = left))
+}
 
 trait 法半树 {
   def 接受索取回馈(number1: Number1, item: Item): Result
   def 没有索取回馈: Result
 }
 case class 左法半树(f: 法, left: Number1) exntends 法半树 {
-  def 接受索取回馈(number1: Number1, item: Item): Result
-  def 没有索取回馈: Result
+  def 接受索取回馈(number1: Number1, item: Item): Result = f.右侧反馈(双白树(left = left, right = number1), item)
+  def 没有索取回馈: Result = f.右侧没有反馈(左白树(left = left))
+}
+case class 只有法半树(f: 法) exntends 法半树 {
+  def 接受索取回馈(number1: Number1, item: Item): Result = ResultPositive(f.计算(左白树(left = left)), item)
+  def 没有索取回馈: Result = ResultZero
 }
 
 trait 法 {
-  def 计算(tree: 白树): Result
+  def 右侧请求(tree: 白树): Result
+  def 右侧反馈(tree: 白树, item: Item): Result
+  def 右侧没有反馈(tree: 白树): Result
+  def 左侧请求(tree: 白树): Result
+  def 左侧反馈(tree: 白树, item: Item): Result
+  def 左侧没有反馈(tree: 白树): Result
+}
+
+case object 加法 {
+  def 右侧请求(tree: 白树): Result
+  def 右侧反馈(tree: 白树, item: Item): Result
+  def 右侧没有反馈(tree: 白树): Result
+  def 左侧请求(tree: 白树): Result
+  def 左侧反馈(tree: 白树, item: Item): Result
+  def 左侧没有反馈(tree: 白树): Result
 }
