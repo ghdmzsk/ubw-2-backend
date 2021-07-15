8c4f8987963c65a3728577dc50cd317f9de8b0a9
水山清风 <djx314@sina.cn>
2021-07-15 07:20:07 +0000

    update a42-stream/src/main/scala/step6/Test1.scala.

--- a42-stream/src/main/scala/step6/Test1.scala
+++ a42-stream/src/main/scala/step6/Test1.scala
@@ -1,3 +1,25 @@
+case class Item(name: String)
+
+trait Result
+case class ResultPositive(tail: Result, item: Item) {
+  override def toString: String = s"($tail, $item)"
+}
+case object ResultZero {
+  override def toString: String = "Zero"
+}
+
+trait Number1
+case class Number1Positive(tail: Number1, item: Item) extends Number1
+case object Nubmer1Zero extends Number1
+
+trait 黑树 {
+  def 计算: Result
+}
+
+trait 白树 {
+  def 向右获取(tree: 法): Result
+}
+
 trait 法 {
-  sdfsdfsdf
+  def 计算(tree: 白树): Result
 }
