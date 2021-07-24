4dc908fe87439c7b7fc103cfd3508b8e4ae4a8b7
djx314 <djx314@sina.cn>
2021-07-25 01:49:11 +0800

    Commit.

---
+++ a42-stream/src/main/scala/step9-数分左右，人分阴阳/Runner1.scala
@@ -0,0 +1,28 @@
+package step9
+
+object Runner1 {
+
+  val itemX   = Item("ItemX")
+  val minus03 = Item("Minus03")
+  val minus02 = Item("Minus02")
+  val minus01 = Item("Minus01")
+  val minus00 = Item("Minus00")
+  val item01  = Item("Item01")
+  val item02  = Item("Item02")
+  val item03  = Item("Item03")
+  val item04  = Item("Item04")
+  val item05  = Item("Item05")
+  val item06  = Item("Item06")
+  val item07  = Item("Item07")
+  val item08  = Item("Item08")
+  val item09  = Item("Item09")
+  val item10  = Item("Item10")
+  val item11  = Item("Item11")
+  val item12  = Item("Item12")
+
+  def main(arr: Array[String]): Unit = {
+    val number1 = NumLeftP(NumLeftP(NumLeftP(NumLeftP(NumLeftP(NumLeftP(NumLeftZero, item01), item02), item03), item04), item05), item06)
+    println(number1.左被索取(MethodL))
+  }
+
+}
--- a42-stream/src/main/scala/step9-数分左右，人分阴阳/建模.scala
+++ a42-stream/src/main/scala/step9-数分左右，人分阴阳/建模.scala
@@ -11,34 +11,35 @@ case object ResultZero extends Result {
 }
 
 trait NumLeft {
-  def 左被索取(cTree: LeftCTree, method: MethodLeft): Result
+  def 左被索取(method: MethodLeft): Result
 }
 case class NumLeftP(tail: NumLeft, head: Item) extends NumLeft {
-  override def 左被索取(cTree: LeftCTree, method: MethodLeft): Result = ResultZero
+  override def 左被索取(method: MethodLeft): Result = method.接收反馈(tail, head)
 }
 case object NumLeftZero extends NumLeft {
-  override def 左被索取(cTree: LeftCTree, method: MethodLeft): Result = ResultZero
+  override def 左被索取(method: MethodLeft): Result = ResultZero
 }
 
 trait NumRight {
-  def 右被索取(cTree: RightCTree, method: MethodRight): Result
+  def 右被索取(method: MethodRight): Result
 }
 case class NumRightP(tail: NumRight, head: Item) extends NumRight {
-  override def 右被索取(cTree: RightCTree, method: MethodRight): Result = ResultZero
+  override def 右被索取(method: MethodRight): Result = ResultZero
 }
 case object NumRightZero extends NumRight {
-  override def 右被索取(cTree: RightCTree, method: MethodRight): Result = ResultZero
+  override def 右被索取(method: MethodRight): Result = ResultZero
 }
 
-case class NumLR(left: NumLeft, right: NumRight) extends NumLeft with NumRight {
-  override def 左被索取(cTree: LeftCTree, method: MethodLeft): Result   = ResultZero
-  override def 右被索取(cTree: RightCTree, method: MethodRight): Result = ResultZero
+case class NumLTree (left: NumLeft)
+
+trait MethodLeft {
+  def 接收反馈(numLeft: NumLeft, item: Item): Result
 }
 
-trait LeftCTree
-
-trait RightCTree
-
-trait MethodLeft
+object MethodL extends MethodLeft {
+  override def 接收反馈(numLeft: NumLeft, item: Item): Result = ResultP(numLeft.左被索取(this), item)
+}
 
 trait MethodRight
+
+
