7c1f7850361b405b98f420f45c0e2abe3bc0c425
djx314 <djx314@sina.cn>
2021-07-26 01:31:55 +0800

    sdfsdfsdf

--- a43-淬炼/src/main/scala/step2/中法零.scala
+++ a43-淬炼/src/main/scala/step2/中法零.scala
@@ -1,6 +1,6 @@
 package step2
 
-/** 中法零，加减法的零，因为既有正法零的特质，也有逆法零的特质，故叫中法零
+/** 中法零，加减法的零，因为既有正法零的特质，也有逆法零的特质，故叫中法零，中法零既不放大，亦不收缩，刚好地把一个左数转化为值
   */
 case class 中法零(tail: () => NumR) extends NumR {
   override def methodL(num: NumL, item: Item): Result = ResultP(num.methodR(tail()), item)
