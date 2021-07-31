f89000368fbba8ef16bd36635c0fbd38913165ec
djx314 <djx314@sina.cn>
2021-07-31 19:14:54 +0800

    sdf

--- a44-对数/src/main/scala/step1/Runner.scala
+++ a44-对数/src/main/scala/step1/Runner.scala
@@ -9,13 +9,14 @@ object Runner extends App {
       zero
   }
   def numRFromInt(n: Int): NumR = {
-    def num1(n1: Int, tail: () => NumR): NumR = {
+    def num1(n1: Int, tail: NumR): NumR = {
       n1 match {
         case n2 if n2 > 0 => 右水(num1(n2 - 1, tail))
-        case 0            => 右风(tail)
+        case 0            => tail
       }
     }
-    lazy val num2: NumR = num1(n, () => num2)
+    lazy val num2: NumR = num1(n, num3)
+    lazy val num3: NumR = 右风(() => num2)
     num2
   }
 
@@ -37,7 +38,7 @@ object Runner extends App {
       override var tail = tail1
       override val head = head1
       override def methodR(num: NumR): Result = {
-        if (zero.isInstanceOf[火]) {
+        if (zero eq this) {
           ResultO
         } else {
           val newZero = 左火(null, head)
@@ -49,17 +50,22 @@ object Runner extends App {
     }
   }
 
-  val number1 = numLFromInt(99)
-  val number2 = numRFromInt(3)
-  val result1 = number1.methodR(number2)
+  /*val number1 = numLFromInt(99)
+  numRFromInt(3)
+  val result1 = number1.methodR(numR)
   val result2 = (math.log(99) / math.log(3)).toInt
-  assert(result1.length == result2)
+  assert(result1.length == result2)*/
 
-  val number3 = numLFromInt(5389)
-  val number4 = numRFromInt(2)
-  val result3 = number3.methodR(number4)
-  val result4 = (math.log(5389) / math.log(2)).toInt
-  println(result3.length)
-  println((math.log(5389) / math.log(2)).toInt)
+  def printlnLog(num1: Int, num2: Int): Unit = {
+    val number3 = numLFromInt(num1)
+    val number4 = numRFromInt(num2)
+    val result3 = number4.methodL(number3, Item("Item01"))
+    val result4 = (math.log(num1) / math.log(num2)).toInt
+    println(s"result left: ${result3.length}, result right: $result4")
+  }
+
+  for (i <- 1 to 12) {
+    printlnLog(math.pow(2, i).toInt, 2)
+  }
 
 }
