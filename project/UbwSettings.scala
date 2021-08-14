29893fc54d4a78e491d25ba39393f43de94b2959
djx314 <djx314@sina.cn>
2021-08-14 13:31:56 +0800

    dffdfgdfg

--- project/UbwSettings.scala
+++ project/UbwSettings.scala
@@ -5,13 +5,17 @@ import sbt.Keys._
 object UbwSettings {
 
   val currentScalaVersion = "2.13.6"
+  val dottyVersion        = "3.0.1"
 
   val setting1 = scalaVersion := currentScalaVersion
   val setting2 = version := "0.0.4-SNAP2020060801"
   val setting3 = organization := "org.xarcher"
   val setting4 = licenses += ("MIT", url("http://opensource.org/licenses/MIT"))
   val setting5 = scalafmtOnCompile := true
+  val setting6 = scalaVersion := dottyVersion
+  val setting7 = scalafmtOnCompile := false
 
   val scalaVersionSettings = Seq(setting1, setting2, setting3, setting4, setting5)
+  val dottyVersionSettings = Seq(setting2, setting3, setting4, setting6, setting7)
 
 }
