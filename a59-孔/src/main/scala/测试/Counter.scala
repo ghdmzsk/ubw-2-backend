cd4a166962a7026cb9283ceaa577de6c8e2c69e8
djx314 <djx314@sina.cn>
2021-09-18 23:33:55 +0800

    M

---
+++ a59-孔/src/main/scala/测试/Counter.scala
@@ -0,0 +1,25 @@
+package 测试
+
+trait Number1 {
+  def method1(number2: Number2): Result
+}
+case class Number1S(tail: Number1) extends Number1 {
+  override def method1(number2: Number2): Result = number2.method2(tail)
+}
+case object Number1T extends Number1 {
+  override def method1(number2: Number2): Result = ResultT
+}
+
+trait Number2 {
+  def method2(number1: Number1): Result
+}
+case class Number2S(tail: Number2) extends Number2 {
+  override def method2(number1: Number1): Result = ResultS(tail.method2(number1))
+}
+case class Number2T(tail: () => Number2) extends Number2 {
+  override def method2(number1: Number1): Result = Number1S(number1).method1(tail())
+}
+
+trait Result
+case class ResultS(tail: Result) extends Result
+case object ResultT              extends Result
