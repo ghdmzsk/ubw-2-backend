a57a9eeb9ab1755235471ce4e737607d4d911509
djx314 <djx314@sina.cn>
2021-09-19 21:27:32 +0800

    sdf

--- a59-孔/src/main/scala/测试/Counter.scala
+++ a59-孔/src/main/scala/测试/Counter.scala
@@ -7,7 +7,7 @@ case class Number1S(tail: Number1) extends Number1 {
   override def method1(number2: Number2): Result = number2.method2(tail)
 }
 case object Number1T extends Number1 {
-  override def method1(number2: Number2): Result = ResultT
+  override def method1(number2: Number2): Result = ResultS(无中生有(number2))
 }
 
 trait Number2 {
@@ -17,7 +17,7 @@ case class Number2S(tail: Number2) extends Number2 {
   override def method2(number1: Number1): Result = ResultS(tail.method2(number1))
 }
 case class Number2T(tail: () => Number2) extends Number2 {
-  override def method2(number1: Number1): Result = Number1S(number1).method1(tail())
+  override def method2(number1: Number1): Result = number1.method1(tail())
 }
 
 trait Result
