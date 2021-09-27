2fe6a00a1d2be608debd0e15fba79fdba7673541
djx314 <djx314@sina.cn>
2021-09-27 14:29:06 +0800

    fgerert

--- a60-四/src/main/scala/四法/Counter7.scala
+++ a60-四/src/main/scala/四法/Counter7.scala
@@ -68,7 +68,7 @@ package 测试1 {
       val yuan        = (i - 1).toDouble / i
       var num: Double = 0
       for (num1 <- 1 to 4000) {
-        num += math.pow(yuan, num1) / num1.toDouble
+        num += math.pow(yuan, num1) / num1
       }
       assert(math.abs(math.log(i) - num) < 0.000001d)
     }
