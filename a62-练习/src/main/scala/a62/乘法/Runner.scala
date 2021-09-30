a16132d8b5d5a9efceb04773b0ca6026e93c8d9a
djx314 <djx314@sina.cn>
2021-10-01 02:20:17 +0800

    sdfegret

--- a62-练习/src/main/scala/a62/乘法/Runner.scala
+++ a62-练习/src/main/scala/a62/乘法/Runner.scala
@@ -1,5 +1,36 @@
 package a62.乘法
 
-object Runner extends App {
-  11
+object Runner {
+  def number1gen(n: Int): Number1 = n match {
+    case n1 if n1 > 0 => Number1S(number1gen(n1 - 1))
+    case 0            => Number1T
+  }
+  def number2gen(n: Int): (Number2, Number2) = {
+    def gen(n1: Int, zero: => Number2): Number2 = n1 match {
+      case n2 if n2 > 0 => Number2S(gen(n2 - 1, zero))
+      case 0            => zero
+    }
+    lazy val number2s: Number2 = gen(n, number2t)
+    lazy val number2t: Number2 = Number2T(() => number2s)
+    (number2s, number2t)
+  }
+  def count(num: Num): Int = num match {
+    case NumS(tail) => count(tail) + 1
+    case NumT       => 0
+  }
+
+  // 乘法和除法都是，number1 先调用，则代表 number1 弱化了，number2 需要给它补充，所以需要函数体有后继的部分
+  // 如果是 number1 后调用，则代表 number1 强大了，需要弱化，number2 就用函数体没有后继的部分
+  def main(arr: Array[String]): Unit = {
+    {
+      val number1      = number1gen(28)
+      val (number2, _) = number2gen(378)
+      assert(count(number1.method1(number2)) == 28 * 378)
+    }
+    {
+      val number1      = number1gen(238)
+      val (_, number2) = number2gen(82)
+      assert(count(number2.method2(number1)) == 238 * 82)
+    }
+  }
 }
--- a62-练习/src/main/scala/a62/除法/Counter.scala
+++ a62-练习/src/main/scala/a62/除法/Counter.scala
@@ -1,21 +1,25 @@
 package a62.除法
 
 trait Number1 {
-  def method1(number2: Number2): Number1
+  def method1(number2: Number2): Num
 }
 case class Number1S(tail: Number1) extends Number1 {
-  override def method1(number2: Number2): Number1 = number2.method2(tail)
+  override def method1(number2: Number2): Num = number2.method2(tail)
 }
 case object Number1T extends Number1 {
-  override def method1(number2: Number2): Number1 = Number1T
+  override def method1(number2: Number2): Num = NumT
 }
 
 trait Number2 {
-  def method2(number1: Number1): Number1
+  def method2(number1: Number1): Num
 }
 case class Number2S(tail: Number2) extends Number2 {
-  override def method2(number1: Number1): Number1 = number1.method1(tail)
+  override def method2(number1: Number1): Num = number1.method1(tail)
 }
 case class Number2T(tail: () => Number2) extends Number2 {
-  override def method2(number1: Number1): Number1 = Number1S(tail().method2(number1))
+  override def method2(number1: Number1): Num = NumS(tail().method2(number1))
 }
+
+trait Num
+case class NumS(tail: Num) extends Num
+case object NumT           extends Num
