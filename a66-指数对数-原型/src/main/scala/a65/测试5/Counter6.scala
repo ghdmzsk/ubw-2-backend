cccda83a4f265793533bf569a18746118c52ce5e
djx314 <djx314@sina.cn>
2021-10-22 03:19:37 +0800

    sdgwerewr

--- a66-指数对数-原型/src/main/scala/a65/测试5/Counter6.scala
+++ a66-指数对数-原型/src/main/scala/a65/测试5/Counter6.scala
@@ -5,28 +5,47 @@ trait Number1 {
   def method3(number2: Number2): Number1
 }
 case class Number1S(tail: () => Number1) extends Number1 {
-  def method1(number2: Number2): Number1          = number2.method2(tail())
-  override def method3(number2: Number2): Number1 = Number1S(() => number2.method2(tail()).method3(number2))
+  override def method1(number2: Number2): Number1 = {
+    println("66" * 100)
+    number2.method2(tail())
+  }
+  override def method3(number2: Number2): Number1 = {
+    println("55" * 100)
+    Number1S(() => number2.method2(tail()).method3(number2))
+  }
 }
 case class Number1T(tail: () => Number1) extends Number1 {
   override def method1(number2: Number2): Number1 = {
-    lazy val aa: Number1 = Number1T(() => aa)
-    aa
+    println("44" * 100)
+    lazy val number1s: Number1 = Number1S(() => number1t)
+    lazy val number1t: Number1 = Number1T(() => number1s)
+    number1t
+    Number1T(() => tail().method1(number2))
+    lazy val number1t2: Number1 = Number1T(() => number1t2)
+    number1t2
+    Number1T(() => tail().method1(number2))
   }
-  override def method3(number2: Number2): Number1 = tail().method3(number2).method1(number2)
+  override def method3(number2: Number2): Number1 = {
+    println("33" * 100)
+    number2.method2(tail().method3(number2))
+  }
 }
 
 trait Number2 {
   def method2(number1: Number1): Number1
 }
-case class Number2SS(tail: () => Number2) extends Number2 {
+case class Number2S(tail: () => Number2) extends Number2 {
   override def method2(number1: Number1): Number1 = Number1S(() => tail().method2(number1))
 }
-case class Number2ST(tail: () => Number2) extends Number2 {
-  override def method2(number1: Number1): Number1 = Number1T(() => tail().method2(number1))
-}
 case class Number2T(tail: () => Number2) extends Number2 {
-  override def method2(number1: Number1): Number1 = number1.method1(tail())
+  override def method2(number1: Number1): Number1 = {
+    println("22" * 100)
+    number1.method1(tail())
+  }
+  def method4(number1: Number1): Number1 = {
+    println("11" * 100)
+    number1.method1(tail()).method3(tail())
+  }
 }
 
 object MathCount {
--- a66-指数对数-原型/src/main/scala/a65/测试5/Runner6.scala
+++ a66-指数对数-原型/src/main/scala/a65/测试5/Runner6.scala
@@ -10,71 +10,85 @@ object Runner6 {
     }
   }
   def number1t(n: Int): Number1 = {
-    lazy val ntZero: Number1 = Number1S(() => ntZero)
-    n match {
-      case n1 if n1 > 0 => Number1T(() => number1t(n1 - 1))
-      case 0            => ntZero
+    def gen(n1: Int, zero: => Number1): Number1 = n1 match {
+      case n2 if n2 > 0 => Number1T(() => gen(n2 - 1, zero))
+      case 0            => zero
     }
+    lazy val number1t: Number1 = gen(n, taZero)
+    lazy val taZero: Number1   = Number1S(() => number1t)
+    number1t
   }
 
-  def number2s(n: Int): (Number2T, Number2SS) = {
-    def gen(n1: Int, zero: => Number2SS): Number2T = n1 match {
+  def number2t(n: Int): Number2T = {
+    def gen(n1: Int, zero: => Number2S): Number2T = n1 match {
       case n2 if n2 > 1 => Number2T(() => gen(n2 - 1, zero))
       case 1            => Number2T(() => zero)
     }
-    lazy val number2t: Number2T   = gen(n, number2sa)
-    lazy val number2sa: Number2SS = Number2SS(() => number2t)
-    (number2t, number2sa)
+    lazy val number2t: Number2T = gen(n, number2s)
+    lazy val number2s: Number2S = Number2S(() => number2t)
+    number2t
   }
-  def number2t(n: Int): (Number2ST, Number2T) = {
-    def gen(n1: Int, zero: => Number2T): Number2ST = n1 match {
-      case n2 if n2 > 1 => Number2ST(() => gen(n2 - 1, zero))
-      case 1            => Number2ST(() => zero)
+  def number2s(n: Int): Number2 = {
+    def gen(n1: Int, zero: => Number2): Number2 = n1 match {
+      case n2 if n2 > 0 => Number2S(() => gen(n2 - 1, zero))
+      case 0            => zero
     }
-    lazy val number2sb: Number2ST = gen(n, number2t)
-    lazy val number2t: Number2T   = Number2T(() => number2sb)
-    (number2sb, number2t)
+    lazy val number2s: Number2 = gen(n, number2t)
+    lazy val number2t: Number2 = Number2T(() => number2s)
+    number2t
   }
-  def counts(number1: Number1): Int = try {
-    number1 match {
-      case Number1S(tail) => counts(tail()) + 1
-      case Number1T(tail) => counts(tail())
-      case _              => 0
-    }
-  } catch {
-    case _: StackOverflowError => 0
+  def counts(number1: Number1): Int = number1 match {
+    case Number1S(tail) =>
+      val num =
+        try {
+          counts(tail())
+        } catch {
+          case e: StackOverflowError => 0
+        }
+      num + 1
+    case Number1T(tail) => 0
   }
-  def countt(number1: Number1): Int = try {
-    number1 match {
-      case Number1S(tail) => countt(tail())
-      case Number1T(tail) => countt(tail()) + 1
-      case _              => 0
-    }
-  } catch {
-    case _: StackOverflowError => 0
+  def countt(number1: Number1): Int = number1 match {
+    case Number1T(tail) =>
+      val num =
+        try {
+          counts(tail())
+        } catch {
+          case e: StackOverflowError => 0
+        }
+      num + 1
+    case Number1S(tail) =>
+      try {
+        countt(tail())
+      } catch {
+        case e: StackOverflowError => 0
+      }
   }
 
   def main(arr: Array[String]): Unit = {
     for {
-      i <- 1 to 200
-      n <- 3 to 3
+      i <- 1 to 2
+      n <- 2 to 3
     } {
-      val result1                        = MathCount.log(n, i)
-      val number1                        = number1s(i)
-      val (number2Positive, number2Zero) = number2s(n)
-      val result2                        = number1.method3(number2Positive)
-      println(i, n, result1, counts(result2), result1 == counts(result2))
+      val result1         = MathCount.log(n, i)
+      val number1         = number1s(i).asInstanceOf[Number1S]
+      val number2Positive = number2t(n)
+      val result2         = number2Positive.method4(number1)
+      val resultNum2      = counts(result2)
+      if (result1 != resultNum2) {
+        println(n, i, resultNum2, result1)
+      }
     }
-    for {
+    /*for {
       i <- 1 to 8
       n <- 1 to 5
     } {
-      val result1                        = MathCount.pow(n, i)
-      val number1                        = number1t(i)
-      val (number2Positive, number2Zero) = number2t(n)
-      val result2                        = number1.method3(number2Positive)
-      println(i, n, result1, countt(result2), result1 == countt(result2))
-    }
+      val result1         = MathCount.pow(n, i)
+      val number1         = number1t(i)
+      val number2Positive = number2s(n)
+      val result2         = number1.method3(number2Positive)
+      println(i, n, result1, counts(result2), result1 == counts(result2))
+    }*/
   }
 
 }
