1079d64233ccff4050d08b4724315e40987d4a94
djx314 <djx314@sina.cn>
2022-02-03 21:49:47 +0800

    112313213

---
+++ a73-带套执行器/src/main/scala/a73/execution/Counter.scala
@@ -0,0 +1,62 @@
+package a73.execution
+
+import scala.annotation.tailrec
+
+trait Exe[+T] {
+  def map[V](s: T => V): Exe[V]          = ExeMap(this, s)
+  def flatMap[V](s: T => Exe[V]): Exe[V] = ExeFlatMap(this, s)
+}
+case class ExeSuccess[+T](value: T)                       extends Exe[T]
+case class ExeMap[U, T](u: Exe[U], func: U => T)          extends Exe[T]
+case class ExeFlatMap[U, T](u: Exe[U], func: U => Exe[T]) extends Exe[T]
+
+object Exe {
+
+  def unsafeRun[T](e: Exe[T]): T = {
+    var v: List[Either[Any => Any, Any => Exe[Any]]] = List.empty
+    @tailrec
+    def loop(exe: Exe[Any]): Unit = exe match {
+      case ExeSuccess(value) =>
+        v = Left((_: Any) => value) :: v
+      case ExeMap(u, func) =>
+        v = Left(func.asInstanceOf[Any => Any]) :: v
+        loop(u)
+      case ExeFlatMap(u, func) =>
+        v = Right(func.asInstanceOf[Any => Exe[Any]]) :: v
+        loop(u)
+    }
+    loop(e)
+
+    var temp: Any = v.head.asInstanceOf[Unit => Any](())
+    v = v.tail
+    var i = 0
+    while (i < v.size - 1) {
+      v.head match {
+        case Left(func) => temp = func(temp)
+        case Right(func) =>
+          val exe = func(temp)
+          temp = unsafeRun(exe)
+      }
+      v = v.tail
+      i += 1
+    }
+
+    temp.asInstanceOf[T]
+  }
+
+}
+
+trait Number[A] {
+  def execute[ToDataType, Result](contexts: Context[ToDataType, A, Result])(t: ToDataType): Result
+}
+case class NumberS[A](tail: () => Number[A], head: A) extends Number[A] {
+  override def execute[ToDataType, Result](context: Context[ToDataType, A, Result])(t: ToDataType): Result = context.bindS(t, tail(), head)
+}
+case class NumberT[A](tail: () => Number[A]) extends Number[A] {
+  override def execute[ToDataType, Result](context: Context[ToDataType, A, Result])(t: ToDataType): Result = context.bindT(t, tail())
+}
+
+trait Context[ToDataType, A, Result] {
+  def bindS(t: ToDataType, current: Number[A], head: A): Result
+  def bindT(t: ToDataType, current: Number[A]): Result
+}
---
+++ a73-带套执行器/src/main/scala/a73/execution/Runner.scala
@@ -0,0 +1,48 @@
+package a73.execution
+
+object Runner {
+
+  def countNumber1(number1: Number1): Int = number1 match {
+    case Number1S(tail, head) => countNumber1(tail) + countNumber2(head) + 1
+    case Number1T             => 0
+  }
+  def countNumber2(number2: Number2): Int = number2 match {
+    case Number2S(tail, head) => countNumber2(tail) + countNumber1(head) - 1
+    case Number2T             => 0
+  }
+
+  def randomDeep: Int   = math.abs(util.Random.nextInt() % 10)
+  def randomLength: Int = math.abs(util.Random.nextInt() % 6)
+
+  def genNumber1(deep: Int): Number1 = {
+    if (deep > randomDeep) {
+      def genLeaf(length: Int): Number1 = if (length > 0) Number1S(genLeaf(length - 1), genNumber2(deep - 1)) else Number1T
+      genLeaf(randomLength)
+    } else
+      Number1T
+  }
+  def genNumber2(deep: Int): Number2 = {
+    if (deep > randomDeep) {
+      def genLeaf(length: Int): Number2 = if (length > 0) Number2S(genLeaf(length - 1), genNumber1(deep - 1)) else Number2T
+      genLeaf(randomLength)
+    } else
+      Number2T
+  }
+
+  def main1(arr: Array[String]): Unit = {
+    for {
+      i <- 8 to 20
+      _ <- 1 to 3
+    } {
+      val num1   = genNumber1(i)
+      val num2   = genNumber2(i + 1)
+      val num3   = num1.method1(num2)
+      val count1 = countNumber1(num1)
+      val count2 = countNumber2(num2)
+      val count3 = countNumber2(num3)
+      // println(s"$count1 + $count2 = $count3 ${count1 + count2 == count3}")
+      assert(count1 + count2 == count3)
+    }
+  }
+
+}
