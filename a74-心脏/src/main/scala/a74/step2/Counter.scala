0a6b6a59d2e3ea2ccf3152b1504c00265bdafd8b
djx314 <djx314@sina.cn>
2022-02-28 02:10:51 +0800

    112313213

--- a74-心脏/src/main/scala/a74/step2/Counter.scala
+++ a74-心脏/src/main/scala/a74/step2/Counter.scala
@@ -1,9 +1,33 @@
 package a74.step2
 
-trait Number1
-case class Number1S(tail: Number1, head: Number2) extends Number1
-case class Number1T(tail: () => Number2)          extends Number1
+import scala.annotation.tailrec
 
-trait Number2
-case class Number2S(tail: Number2, head: Number1) extends Number2
-case class Number2T(tail: () => Number1)          extends Number2
+trait Number1[-S, +T]
+case class Number1S[-S, +T](tail: Number1[S, T], head: Number2[T, S]) extends Number1[S, T]
+case class Number1T[-S, +T](tail: S => T)                             extends Number1[S, T]
+
+trait Number2[+S, -T]
+case class Number2S[+S, -T](tail: Number2[S, T], head: Number1[T, S]) extends Number2[S, T]
+case class Number2T[+S, -T](tail: T => S)                             extends Number2[S, T]
+
+trait Number3[-S, +T] {
+  def func(s: S): T
+}
+case class Number3S[-S, +T, A](head: S => A, tail: Number3[A, T]) extends Number3[S, T] {
+  override def func(s: S): T = tail.func(head(s))
+}
+case object Number3T extends Number3[Nothing, Any] {
+  def func(s: Nothing): Any = s
+}
+
+object Number3 {
+  def exec[S, T](number3: Number3[S, T]): S => T = s => execImpl(s, number3)
+
+  @tailrec
+  def execImpl[S, T](s: S, number3: Number3[S, T]): T = {
+    number3 match {
+      case Number3S(head, tail) => execImpl(head(s), tail)
+      case b @ Number3T         => b.func(s)
+    }
+  }
+}
